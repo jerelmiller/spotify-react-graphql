1065c8087a1c064c0e83a512095a1080eb3b98c6
Jerel Miller <jerelmiller@gmail.com>
2018-10-02 23:26:23 -0700

    Convert to styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import styled, { keyframes } from 'styled-components'
+
+const logoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Container = styled.div`
+  text-align: center;
+`
+
+const Logo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 40vmin;
+`
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const Link = styled.a`
+  color: #61dafb;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Container>
+        <Header className="App-header">
+          <Logo src={logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
+          <Link
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </Link>
+        </Header>
+      </Container>
     )
   }
 }
