c45d16ce9c2613b4d88d0077bde980020b8488a1
Jerel Miller <jerelmiller@gmail.com>
2018-10-03 00:52:34 -0700

    Add footer and use grid template areas

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,26 @@ const Container = styled.div`
   text-align: center;
   height: 100vh;
   display: grid;
+  grid-template-areas:
+    'sidebar main'
+    'footer footer';
   grid-template-columns: 220px 1fr;
+  grid-template-rows: 1fr auto;
 `
 
 const Sidebar = styled.aside`
+  grid-area: sidebar;
   background: #121212;
 `
 
 const Main = styled.main`
+  grid-area: main;
   background: #181818;
+`
+
+const Footer = styled.footer`
+  grid-area: footer;
+  background: #282828;
 `
 
 class App extends Component {
@@ -25,6 +36,7 @@ class App extends Component {
         <Container>
           <Sidebar />
           <Main />
+          <Footer />
         </Container>
       </>
     )
