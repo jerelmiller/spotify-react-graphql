4ddbb2dae0b801ae853715acec758aed4069af4b
Jerel Miller <jerelmiller@gmail.com>
2018-10-03 23:35:04 -0700

    Finer grained client creation

--- client/src/config/apollo.js
+++ client/src/config/apollo.js
@@ -1,7 +1,24 @@
-import ApolloClient from 'apollo-boost'
+import { ApolloLink } from 'apollo-link'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+
+const cache = new InMemoryCache()
+
+const httpLink = new HttpLink({
+  uri: `${process.env.REACT_APP_API_HOST}/graphql`
+})
+
+const setAuthorizationLink = setContext(() => ({
+  headers: {
+    Authorization: localStorage.getItem('token') || null
+  }
+}))
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_API_HOST}/graphql`
+  cache,
+  link: ApolloLink.from([setAuthorizationLink, httpLink])
 })
 
 export default client
