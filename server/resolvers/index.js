9bf62343872b022e3731d4b52f2699e0b2cb1062
Jerel Miller <jerelmiller@gmail.com>
2018-10-04 00:07:22 -0700

    Start working on current user data

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,6 +1,16 @@
+import { prop } from '../utils/fp'
+
 const resolvers = {
   Query: {
-    artist: () => ({ id: 1 })
+    artist: () => ({ id: 1 }),
+    viewer: () => ({}) // dummy to allow Viewer type to work below
+  },
+  User: {
+    displayName: prop('display_name')
+  },
+  Viewer: {
+    user: async (_source, _args, { dataSources }) =>
+      dataSources.spotifyAPI.getCurrentUser()
   }
 }
 
