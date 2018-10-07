43091b261e789630bc592ddc03e713e5be8d3c46
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 16:53:27 -0700

    Remove token when unauthenticated

--- client/src/config/apollo.js
+++ client/src/config/apollo.js
@@ -2,16 +2,39 @@ import { ApolloLink } from 'apollo-link'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
 import { setContext } from 'apollo-link-context'
 
+const TOKEN_KEY = 'token'
+
 const cache = new InMemoryCache()
+
+const retryAuthLink = onError(
+  ({ graphQLErrors, networkErrors, operation, forward }) => {
+    // don't handle unauthenticated when there is no token or a network error
+    if (Boolean(networkErrors) || !localStorage.getItem(TOKEN_KEY)) {
+      return
+    }
+
+    for (let err of graphQLErrors) {
+      switch (err.extensions.code) {
+        case 'UNAUTHENTICATED':
+          // TODO refresh the token
+          localStorage.removeItem(TOKEN_KEY)
+          return forward(operation)
+        default:
+          return
+      }
+    }
+  }
+)
 
 const httpLink = new HttpLink({
   uri: `${process.env.REACT_APP_API_HOST}/graphql`
 })
 
 const setAuthorizationLink = setContext(() => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem(TOKEN_KEY)
 
   return {
     headers: {
@@ -22,7 +45,7 @@ const setAuthorizationLink = setContext(() => {
 
 const client = new ApolloClient({
   cache,
-  link: ApolloLink.from([setAuthorizationLink, httpLink])
+  link: ApolloLink.from([setAuthorizationLink, retryAuthLink, httpLink])
 })
 
 export default client
