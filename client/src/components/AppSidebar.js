6ea1963d00274df8a2f5e8ac0eba5f00082b7ebc
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 17:52:20 -0700

    More styling on sidebar

--- client/src/components/AppSidebar.js
+++ client/src/components/AppSidebar.js
@@ -4,6 +4,7 @@ import UserAvatar from './UserAvatar'
 import NavLink from './NavLink'
 import UnstyledList from './UnstyledList'
 import styled from 'styled-components'
+import { color } from 'styles/utils'
 
 const Sidebar = styled.aside`
   grid-area: sidebar;
@@ -22,8 +23,9 @@ const Title = styled.h5`
   padding: 0 2rem;
   font-weight: 300;
   margin: 0;
-  margin-bottom: 0.25rem;
+  margin-bottom: 0.5rem;
   letter-spacing: 1px;
+  border-left: 0.375rem transparent;
 `
 
 const NavSection = ({ children, title }) => (
@@ -39,26 +41,49 @@ const SidebarLink = styled(NavLink)`
   display: block;
   font-size: 0.95rem;
   color: #858585;
-  padding: 0.25rem 2rem;
-  transition: color 0.15s ease-in-out;
+  padding: 0 1.5rem;
+  transition: color 0.15s ease-in-out, border-left 0.3s ease-in-out;
+  border-left: 0.375rem solid transparent;
+
+  &:not(:last-child) {
+    margin-bottom: 0.25rem;
+  }
 
   &:hover,
   &.active {
     color: #fff;
   }
+
+  &.active {
+    border-left-color: ${color('green')};
+  }
+`
+
+const Li = styled.li`
+  &:not(:last-child) {
+    margin-bottom: 0.5rem;
+  }
 `
 
 const Link = ({ children, to }) => (
-  <li>
+  <Li>
     <SidebarLink to={to}>{children}</SidebarLink>
-  </li>
+  </Li>
 )
+
+const AvatarContainer = styled.div`
+  padding: 1rem 1.5rem;
+  margin-bottom: 1rem;
+  border-left: 0.375rem solid transparent;
+`
 
 const AppSidebar = ({ loading, viewer }) => (
   <Sidebar>
     {loading || (
       <>
-        <UserAvatar user={viewer.user} />
+        <AvatarContainer>
+          <UserAvatar user={viewer.user} />
+        </AvatarContainer>
         <NavSection>
           <Link to="browse">Browse</Link>
         </NavSection>
