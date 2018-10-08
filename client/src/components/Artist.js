48e72766f30e4c1aefeabc2c3af050e48745f17d
Jerel Miller <jerelmiller@gmail.com>
2018-10-08 00:14:22 -0700

    Add artist image

--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import gql from 'graphql-tag'
+import LazyImage from 'components/LazyImage'
 import styled from 'styled-components'
 
 const Container = styled.div`
   text-align: center;
 `
 
-const Artist = ({ artist }) => <Container>{artist.name}</Container>
+const ArtistImage = styled(LazyImage)`
+  display: block;
+  margin-bottom: 0.5rem;
+`
+
+const Artist = ({ artist }) => {
+  const image = artist.images[0]
+
+  return (
+    <Container>
+      <ArtistImage src={image.url} />
+      {artist.name}
+    </Container>
+  )
+}
 
 Artist.fragments = {
   artist: gql`
     fragment Artist_artist on Artist {
       id
       name
+
+      images {
+        url
+      }
     }
   `
 }
