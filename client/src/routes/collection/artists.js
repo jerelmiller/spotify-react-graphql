aa96ca0c4e903b4fa1afb553712e67f5388f44b6
Jerel Miller <jerelmiller@gmail.com>
2018-10-08 00:10:53 -0700

    Start working on artist page

--- client/src/routes/collection/artists.js
+++ client/src/routes/collection/artists.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Artist from 'components/Artist'
 import gql from 'graphql-tag'
 import PageTitle from 'components/PageTitle'
+import TileGrid from 'components/TileGrid'
 import { Query } from 'react-apollo'
 
 const Artists = () => (
@@ -12,18 +14,26 @@ const Artists = () => (
             edges {
               node {
                 id
-                name
+                ...Artist_artist
               }
             }
           }
         }
       }
+
+      ${Artist.fragments.artist}
     `}
     variables={{ limit: 50 }}
   >
     {({ loading, data: { viewer } }) => (
       <>
         <PageTitle>Artists</PageTitle>
+        <TileGrid minWidth="200px">
+          {loading ||
+            viewer.followedArtists.edges.map(({ node }) => (
+              <Artist key={node.id} artist={node} />
+            ))}
+        </TileGrid>
       </>
     )}
   </Query>
