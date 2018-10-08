9f29a1d93bc915e567fc812a7f10c3e6a2612499
Jerel Miller <jerelmiller@gmail.com>
2018-10-08 01:33:43 -0700

    Make avatar a lazy image

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import LazyImage from './LazyImage'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
 
-const Img = styled.img`
+const Img = styled(LazyImage)`
   border-radius: 50%;
   width: 2rem;
   height: 2rem;
+  padding-bottom: 0;
 `
 
 const Avatar = ({ image }) => <Img src={image.url} alt="avatar" />
