8f1a19a667b7ea5c1a824d48feced35c2d176d3f
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 19:32:43 -0700

    Update PlayIcon to use FeatherSVG

--- client/src/components/PlayIcon.js
+++ client/src/components/PlayIcon.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import styled from 'styled-components'
-import { prop } from 'utils/fp'
-
-const Svg = styled.svg`
-  width: ${prop('size')};
-  height: ${prop('size')};
-`
+import FeatherSVG from './FeatherSVG'
 
 const PlayIcon = props => (
-  <Svg viewBox="0 0 85 100" {...props}>
-    <path
-      fill="currentColor"
-      d="M81 44.6c5 3 5 7.8 0 10.8L9 98.7c-5 3-9 .7-9-5V6.3c0-5.7 4-8 9-5l72 43.3z"
-    >
-      <title>PLAY</title>
-    </path>
-  </Svg>
+  <FeatherSVG {...props}>
+    <title>play</title>
+    <polygon points="5 3 19 12 5 21 5 3" />
+  </FeatherSVG>
 )
 
 export default PlayIcon
