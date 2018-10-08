dc4bd3d0014cecc6a85b79e5a99679376e87f7c5
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 19:38:58 -0700

    Add music icon

---
+++ client/src/components/MusicIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import FeatherSVG from './FeatherSVG'
+
+const MusicIcon = props => (
+  <FeatherSVG {...props}>
+    <path d="M9 17H5a2 2 0 0 0-2 2 2 2 0 0 0 2 2h2a2 2 0 0 0 2-2zm12-2h-4a2 2 0 0 0-2 2 2 2 0 0 0 2 2h2a2 2 0 0 0 2-2z" />
+    <polyline points="9 17 9 5 21 3 21 15" />
+  </FeatherSVG>
+)
+
+export default MusicIcon
