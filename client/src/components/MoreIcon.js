347c0bebd23ad554ca65b0876471a4281509a2a3
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 19:44:35 -0700

    Add MoreIcon

---
+++ client/src/components/MoreIcon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FeatherSVG from './FeatherSVG'
+
+const MoreIcon = props => (
+  <FeatherSVG {...props}>
+    <circle cx="12" cy="12" r="1" />
+    <circle cx="19" cy="12" r="1" />
+    <circle cx="5" cy="12" r="1" />
+  </FeatherSVG>
+)
+
+export default MoreIcon
