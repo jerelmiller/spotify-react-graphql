8ede01480d48eed9d1e7ad120feb181652e46894
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 22:42:10 -0700

    Add an addUnits helper function

--- client/src/styles/utils.js
+++ client/src/styles/utils.js
@@ -1,5 +1,9 @@
 import { curry } from 'utils/fp'
 
+export const addUnits = curry(
+  (unit, value) => (typeof value === 'number' ? value + unit : value)
+)
+
 export const color = curry((name, { theme }) => theme.colors[name])
 
 export const textColor = curry(
