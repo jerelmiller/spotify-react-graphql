77d1d7f4c11698724e88832e75d36410ef2250c6
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 20:14:28 -0700

    More styling on track

--- client/src/components/Track.js
+++ client/src/components/Track.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Duration from './Duration'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
 import MusicIcon from './MusicIcon'
+import MoreIcon from './MoreIcon'
+import ExplicitBadge from './ExplicitBadge'
+import { Link } from '@reach/router'
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: auto 1fr auto auto;
-  grid-gap: 1rem;
+  grid-column-gap: 1rem;
   align-items: center;
   border-radius: 2px;
   padding: 0.75rem;
@@ -17,10 +21,47 @@ const Container = styled.div`
   }
 `
 
+const TrackName = styled.span`
+  font-weight: 300;
+`
+
+const TrackDuration = styled(Duration)`
+  font-weight: 300;
+  width: 3rem;
+  text-align: right;
+`
+
+const Info = styled.div`
+  grid-column: 2 / -1;
+  color: hsla(0, 0%, 100%, 0.6);
+`
+
+const ItemLink = styled(Link)`
+  border-bottom: 1px solid transparent;
+  transition: all 0.2s ease-in-out;
+  font-size: 0.85rem;
+  font-weight: 300;
+
+  &:hover {
+    color: #fff;
+    border-bottom-color: #fff;
+  }
+`
+
 const Track = ({ track }) => (
   <Container>
     <MusicIcon size="1.25rem" strokeWidth={1} />
-    {track.name}
+    <TrackName>{track.name}</TrackName>
+    <MoreIcon size="1.25rem" />
+    <TrackDuration duration={track.duration} />
+    <Info>
+      {track.explicit && <ExplicitBadge />}{' '}
+      <ItemLink to={`/artists/${track.artists[0].id}`}>
+        {track.artists[0].name}
+      </ItemLink>{' '}
+      &middot;{' '}
+      <ItemLink to={`/albums/${track.album.id}`}>{track.album.name}</ItemLink>
+    </Info>
   </Container>
 )
 
@@ -29,6 +70,8 @@ Track.fragments = {
     fragment Track_track on Track {
       id
       name
+      duration
+      explicit
       album {
         id
         name
