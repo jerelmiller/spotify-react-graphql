78bc66992d60e55b115febd239503d13132ae549
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 20:24:21 -0700

    Add text color helper

--- client/src/styles/utils.js
+++ client/src/styles/utils.js
@@ -1,3 +1,7 @@
 import { curry } from 'utils/fp'
 
 export const color = curry((name, { theme }) => theme.colors[name])
+
+export const textColor = curry(
+  (name, { theme }) => theme.typography.colors[name]
+)
