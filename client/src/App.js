11ea5ec9aedda5599ca4027454314340d40a09c9
Jerel Miller <jerelmiller@gmail.com>
2018-10-07 17:37:51 -0700

    Wrap app in ThemeProvider

--- client/src/App.js
+++ client/src/App.js
@@ -8,29 +8,33 @@ import Browse from './routes/browse'
 import BrowseDiscover from './routes/browse/discover'
 import BrowseFeatured from './routes/browse/featured'
 import SetToken from './routes/set-token'
+import theme from './styles/theme'
 import { ApolloProvider } from 'react-apollo'
+import { ThemeProvider } from 'styled-components'
 import { Redirect, Router } from '@reach/router'
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <GlobalStyle />
-        <Auth>
-          <AppLayout>
-            <Router>
-              <Redirect noThrow from="/" to="browse/featured" />
-              <Login path="login" />
-              <SetToken path="set-token" />
-              <Browse path="browse">
-                <Redirect noThrow from="/" to="featured" />
-                <BrowseDiscover path="discover" />
-                <BrowseFeatured path="featured" />
-              </Browse>
-            </Router>
-          </AppLayout>
-        </Auth>
-      </ApolloProvider>
+      <ThemeProvider theme={theme}>
+        <ApolloProvider client={client}>
+          <GlobalStyle />
+          <Auth>
+            <AppLayout>
+              <Router>
+                <Redirect noThrow from="/" to="browse/featured" />
+                <Login path="login" />
+                <SetToken path="set-token" />
+                <Browse path="browse">
+                  <Redirect noThrow from="/" to="browse/featured" />
+                  <BrowseDiscover path="discover" />
+                  <BrowseFeatured path="featured" />
+                </Browse>
+              </Router>
+            </AppLayout>
+          </Auth>
+        </ApolloProvider>
+      </ThemeProvider>
     )
   }
 }
