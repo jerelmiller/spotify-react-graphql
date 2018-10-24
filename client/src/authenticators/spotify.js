13e24114751adc608878c7f5d074ad8f7428568d
Jerel Miller <jerelmiller@gmail.com>
2018-10-24 16:26:32 -0700

    Restore a bit differently

--- client/src/authenticators/spotify.js
+++ client/src/authenticators/spotify.js
@@ -2,7 +2,7 @@ import { createAuthenticator } from 'redux-simple-auth'
 
 export default createAuthenticator({
   name: 'spotify',
-  restore: data => Promise.resolve(data),
+  restore: data => (data.token ? Promise.resolve(data) : Promise.reject()),
   authenticate: token => {
     if (Boolean(token)) {
       return Promise.resolve({ token })
