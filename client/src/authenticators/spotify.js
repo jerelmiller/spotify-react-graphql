b6870781baff703eef8f9e3f0f7915b7c8833f14
Jerel Miller <jerelmiller@gmail.com>
2018-10-24 16:35:17 -0700

    Always resolve data on restore

--- client/src/authenticators/spotify.js
+++ client/src/authenticators/spotify.js
@@ -2,7 +2,7 @@ import { createAuthenticator } from 'redux-simple-auth'
 
 export default createAuthenticator({
   name: 'spotify',
-  restore: data => (data.token ? Promise.resolve(data) : Promise.reject()),
+  restore: data => Promise.resolve(data),
   authenticate: token => {
     if (Boolean(token)) {
       return Promise.resolve({ token })
