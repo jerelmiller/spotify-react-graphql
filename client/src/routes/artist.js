3ae5341432e271fb614570e90d4a3b0234c90b29
Jerel Miller <jerelmiller@gmail.com>
2018-10-24 15:17:45 -0700

    Start working on artist cover photo

--- client/src/routes/artist.js
+++ client/src/routes/artist.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import LazyImage from 'components/LazyImage'
 import gql from 'graphql-tag'
+import styled from 'styled-components'
 import { Query } from 'react-apollo'
+import { prop } from 'utils/fp'
+
+const Container = styled.div`
+  position: relative;
+`
+
+const CoverPhoto = styled(LazyImage)`
+  position: absolute;
+  top: -2rem;
+  left: -2rem;
+  width: calc(100% + 4rem);
+  z-index: -1;
+  background-image: url(${prop('src')});
+  background-size: cover;
+`
 
 const Album = ({ artistId }) => (
   <Query
@@ -9,12 +26,25 @@ const Album = ({ artistId }) => (
         artist(id: $artistId) {
           id
           name
+
+          images {
+            url
+            width
+            height
+          }
         }
       }
     `}
     variables={{ artistId }}
   >
-    {({ loading, data: { artist } }) => loading || <div>{artist.name}</div>}
+    {({ loading, data: { artist } }) =>
+      loading || (
+        <Container>
+          <CoverPhoto src={artist.images[0].url} as="div" />
+          {artist.name}
+        </Container>
+      )
+    }
   </Query>
 )
 
