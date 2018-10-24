8ebc3fb6080784baf35652214287be84f695820a
Jerel Miller <jerelmiller@gmail.com>
2018-10-24 16:22:43 -0700

    Add spotify authenticator and authorizer config

--- client/src/config/store.js
+++ client/src/config/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
 import { createAuthMiddleware } from 'redux-simple-auth'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import spotify from 'authenticators/spotify'
 import rootReducer from 'reducers'
 
 const authMiddleware = createAuthMiddleware({
-  authenticator: () => {}
+  authenticator: spotify,
+  authorizer: ({ token }, header) => {
+    if (token) {
+      header('Authorization', `Bearer ${token}`)
+    }
+  }
 })
 
 const store = createStore(
