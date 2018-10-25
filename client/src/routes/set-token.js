a1b3ba8ff899f5d920c224e42f76a05af550018b
Jerel Miller <jerelmiller@gmail.com>
2018-10-24 17:01:22 -0700

    💩 auth. Good enough for now

--- client/src/routes/set-token.js
+++ client/src/routes/set-token.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import Component from '@reactions/component'
 import QueryParams from 'components/QueryParams'
+import Session from 'components/Session'
+import { authenticate } from 'redux-simple-auth'
 import { Redirect } from '@reach/router'
+import { connect } from 'react-redux'
 
-const SetToken = () => (
+const SetToken = ({ authenticate }) => (
   <QueryParams>
-    {query => {
-      localStorage.setItem('token', query.get('token'))
-
-      return <Redirect noThrow to="/browse/featured" />
-    }}
+    {query => (
+      <Component didMount={() => authenticate('spotify', query.get('token'))}>
+        {() => (
+          <Session>
+            {({ authenticated }) =>
+              authenticated ? <Redirect noThrow to="/browse/featured" /> : null
+            }
+          </Session>
+        )}
+      </Component>
+    )}
   </QueryParams>
 )
 
-export default SetToken
+export default connect(
+  null,
+  { authenticate }
+)(SetToken)
