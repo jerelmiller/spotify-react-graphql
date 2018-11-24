26efebca1ab0f2b35bedb72edf31f1a9aad9b17b
Jerel Miller <jmiller@newrelic.com>
2018-11-24 14:48:53 -0800

    Start working on login page styles

--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import SpotifyLogo from 'components/SpotifyLogo'
+import FlexContainer from 'components/FlexContainer'
+
+const Container = styled(FlexContainer).attrs({
+  alignItems: 'center',
+  justifyContent: 'center'
+})`
+  height: 100vh;
+`
+
+const Logo = styled(SpotifyLogo)`
+  display: block;
+  margin-bottom: 2.5rem;
+`
 
 const Login = () => (
-  <div>
-    <a href={`${process.env.REACT_APP_API_HOST}/oauth/init`}>
-      Login with Spotify
-    </a>
-  </div>
+  <Container>
+    <FlexContainer alignItems="center" direction="column">
+      <Logo size="medium" />
+      <a href={`${process.env.REACT_APP_API_HOST}/oauth/init`}>
+        Login with Spotify
+      </a>
+    </FlexContainer>
+  </Container>
 )
 
 export default Login
