96a1f8738d8f1cbc73e318eef37ab99019f087ed
Jerel Miller <jmiller@newrelic.com>
2018-11-24 14:26:58 -0800

    Update App to use session hook

--- client/src/App.js
+++ client/src/App.js
@@ -10,42 +10,40 @@ import Browse from './routes/browse'
 import BrowseDiscover from './routes/browse/discover'
 import BrowseFeatured from './routes/browse/featured'
 import LoggedOut from './routes/logged-out'
-import Session from './components/Session'
 import SetToken from './routes/set-token'
 import Tracks from './routes/collection/tracks'
+import useSession from './hooks/useSession'
 import { Redirect, Router } from '@reach/router'
 
-const App = () => (
-  <Session>
-    {({ authenticated }) =>
-      authenticated ? (
-        <AppLayout>
-          <Router primary={false}>
-            <Redirect noThrow from="/" to="browse/featured" />
-            <Album path="albums/:albumId" />
-            <Artist path="artists/:artistId" />
-            <Browse path="browse">
-              <Redirect noThrow from="/" to="browse/featured" />
-              <BrowseDiscover path="discover" />
-              <BrowseFeatured path="featured" />
-            </Browse>
-            <Albums path="collection/albums" />
-            <Artists path="collection/artists" />
-            <Tracks path="collection/tracks" />
-            <Logout path="logout" />
+const App = () => {
+  const { isAuthenticated } = useSession()
 
-            <SetToken path="set-token" />
-          </Router>
-        </AppLayout>
-      ) : (
-        <Router primary={false}>
-          <Login path="login" />
-          <SetToken path="set-token" />
-          <LoggedOut default />
-        </Router>
-      )
-    }
-  </Session>
-)
+  return isAuthenticated ? (
+    <AppLayout>
+      <Router primary={false}>
+        <Redirect noThrow from="/" to="browse/featured" />
+        <Album path="albums/:albumId" />
+        <Artist path="artists/:artistId" />
+        <Browse path="browse">
+          <Redirect noThrow from="/" to="browse/featured" />
+          <BrowseDiscover path="discover" />
+          <BrowseFeatured path="featured" />
+        </Browse>
+        <Albums path="collection/albums" />
+        <Artists path="collection/artists" />
+        <Tracks path="collection/tracks" />
+        <Logout path="logout" />
+
+        <SetToken path="set-token" />
+      </Router>
+    </AppLayout>
+  ) : (
+    <Router primary={false}>
+      <Login path="login" />
+      <SetToken path="set-token" />
+      <LoggedOut default />
+    </Router>
+  )
+}
 
 export default App
