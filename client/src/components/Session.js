704f5da2c49a15e726f3e402c10e167a9b242dec
Jerel Miller <jmiller@newrelic.com>
2018-11-24 13:38:17 -0800

    Wrap Session in SessionContext

--- client/src/components/Session.js
+++ client/src/components/Session.js
@@ -1,8 +1,13 @@
+import React from 'react'
 import { connect } from 'react-redux'
 import { getIsAuthenticated, getIsRestored } from 'redux-simple-auth'
+import SessionContext from './SessionContext'
 
-const Session = ({ children, restored, ...props }) =>
-  restored ? children(props) : null
+const Session = ({ children, restored, ...props }) => (
+  <SessionContext.Provider value={{ ...props, restored }}>
+    {restored ? children(props) : null}
+  </SessionContext.Provider>
+)
 
 const mapStateToProps = state => ({
   authenticated: getIsAuthenticated(state),
