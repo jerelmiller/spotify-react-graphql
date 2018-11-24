aa2d203a20bbe4849c444d6e50222fac482e5752
Jerel Miller <jmiller@newrelic.com>
2018-11-24 14:52:41 -0800

    Add button component. Update login button to use button

--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import SpotifyLogo from 'components/SpotifyLogo'
 import FlexContainer from 'components/FlexContainer'
+import Button from 'components/Button'
 
 const Container = styled(FlexContainer).attrs({
   alignItems: 'center',
@@ -19,9 +20,9 @@ const Login = () => (
   <Container>
     <FlexContainer alignItems="center" direction="column">
       <Logo size="medium" />
-      <a href={`${process.env.REACT_APP_API_HOST}/oauth/init`}>
+      <Button as="a" href={`${process.env.REACT_APP_API_HOST}/oauth/init`}>
         Login with Spotify
-      </a>
+      </Button>
     </FlexContainer>
   </Container>
 )
