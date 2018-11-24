4f5d3ca8cb7eede0b145faa787ae56703b04abe4
Jerel Miller <jmiller@newrelic.com>
2018-11-24 13:41:49 -0800

    Revert "Wrap Session in SessionContext"
    This reverts commit 704f5da2c49a15e726f3e402c10e167a9b242dec.

--- client/src/components/Session.js
+++ client/src/components/Session.js
@@ -1,13 +1,8 @@
-import React from 'react'
 import { connect } from 'react-redux'
 import { getIsAuthenticated, getIsRestored } from 'redux-simple-auth'
-import SessionContext from './SessionContext'
 
-const Session = ({ children, restored, ...props }) => (
-  <SessionContext.Provider value={{ ...props, restored }}>
-    {restored ? children(props) : null}
-  </SessionContext.Provider>
-)
+const Session = ({ children, restored, ...props }) =>
+  restored ? children(props) : null
 
 const mapStateToProps = state => ({
   authenticated: getIsAuthenticated(state),
