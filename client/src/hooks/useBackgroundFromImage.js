784fc4eb090dbd77c706e8c5b4b59f3600bec633
Jerel Miller <jerelmiller@gmail.com>
2018-11-24 19:08:39 -0800

    Replace vibrant with color-thief

--- client/src/hooks/useBackgroundFromImage.js
+++ client/src/hooks/useBackgroundFromImage.js
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react'
 import useBackgroundColor from './useBackgroundColor'
-import * as Vibrant from 'node-vibrant'
+import ColorThief from 'color-thief'
+import { rgb, shade } from 'polished'
 
-const useBackgroundFromImage = src => {
+const useBackgroundFromImage = ref => {
   const [color, setColor] = useState()
   useBackgroundColor(color)
 
-  useEffect(
-    () => {
-      if (src) {
-        Vibrant.from(src)
-          .getPalette()
-          .then(
-            pallete => console.log(pallete) || setColor(pallete.Muted.getHex())
-          )
+  useEffect(() => {
+    const image = new Image()
+
+    if (ref.current) {
+      image.crossOrigin = true
+      image.onload = () => {
+        const colorThief = new ColorThief()
+        const color = rgb(...colorThief.getColor(image))
+        setColor(shade(0.2, color))
       }
-    },
-    [src]
-  )
+      image.src = ref.current.src
+    }
+
+    return () => (image.onload = null)
+  })
 }
 
 export default useBackgroundFromImage
