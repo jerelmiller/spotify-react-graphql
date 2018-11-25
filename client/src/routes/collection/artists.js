211eab36ef53d07e8c06e26fd72ecfc82fde71b4
Jerel Miller <jerelmiller@gmail.com>
2018-11-24 17:19:35 -0800

    Add dynamic background to artists

--- client/src/routes/collection/artists.js
+++ client/src/routes/collection/artists.js
@@ -3,40 +3,45 @@ import ArtistTile from 'components/ArtistTile'
 import gql from 'graphql-tag'
 import PageTitle from 'components/PageTitle'
 import TileGrid from 'components/TileGrid'
+import useBackgroundColor from 'hooks/useBackgroundColor'
 import { Query } from 'react-apollo'
 
-const Artists = () => (
-  <Query
-    query={gql`
-      query ArtistsQuery($limit: Int!, $after: String) {
-        viewer {
-          followedArtists(limit: $limit, after: $after) {
-            edges {
-              node {
-                id
-                ...Artist_artist
+const Artists = () => {
+  useBackgroundColor('#4E361C')
+
+  return (
+    <Query
+      query={gql`
+        query ArtistsQuery($limit: Int!, $after: String) {
+          viewer {
+            followedArtists(limit: $limit, after: $after) {
+              edges {
+                node {
+                  id
+                  ...Artist_artist
+                }
               }
             }
           }
         }
-      }
 
-      ${ArtistTile.fragments.artist}
-    `}
-    variables={{ limit: 50 }}
-  >
-    {({ loading, data: { viewer } }) => (
-      <>
-        <PageTitle>Artists</PageTitle>
-        <TileGrid minWidth="160px">
-          {loading ||
-            viewer.followedArtists.edges.map(({ node }) => (
-              <ArtistTile key={node.id} artist={node} />
-            ))}
-        </TileGrid>
-      </>
-    )}
-  </Query>
-)
+        ${ArtistTile.fragments.artist}
+      `}
+      variables={{ limit: 50 }}
+    >
+      {({ loading, data: { viewer } }) => (
+        <>
+          <PageTitle>Artists</PageTitle>
+          <TileGrid minWidth="160px">
+            {loading ||
+              viewer.followedArtists.edges.map(({ node }) => (
+                <ArtistTile key={node.id} artist={node} />
+              ))}
+          </TileGrid>
+        </>
+      )}
+    </Query>
+  )
+}
 
 export default Artists
