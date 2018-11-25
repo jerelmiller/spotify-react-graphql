86964f3b586c09f1f17702e1838c88cd050d4e65
Jerel Miller <jerelmiller@gmail.com>
2018-11-24 20:06:52 -0800

    Switch to using source

--- client/src/hooks/useBackgroundFromImage.js
+++ client/src/hooks/useBackgroundFromImage.js
@@ -3,21 +3,21 @@ import useBackgroundColor from './useBackgroundColor'
 import ColorThief from 'color-thief'
 import { rgb, shade } from 'polished'
 
-const useBackgroundFromImage = ref => {
+const useBackgroundFromImage = src => {
   const [color, setColor] = useState()
   useBackgroundColor(color)
 
   useEffect(() => {
     const img = new Image()
 
-    if (ref.current) {
+    if (src) {
       img.crossOrigin = true
       img.onload = () => {
         const colorThief = new ColorThief()
         const color = rgb(...colorThief.getColor(img))
         setColor(shade(0.3, color))
       }
-      img.src = ref.current.src
+      img.src = src
     }
 
     return () => {
