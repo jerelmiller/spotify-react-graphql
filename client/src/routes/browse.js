e8924ffed9a469e690ef5cc14291fe6d4192aaa9
Jerel Miller <jerelmiller@gmail.com>
2018-11-27 23:58:26 -0800

    Move header back to browse

--- client/src/routes/browse.js
+++ client/src/routes/browse.js
@@ -3,14 +3,16 @@ import TabNav from 'components/TabNav'
 
 const RouteBrowse = ({ children }) => (
   <>
-    <TabNav>
-      <TabNav.NavItem to="featured">Featured</TabNav.NavItem>
-      <TabNav.NavItem to="podcasts">Podcasts</TabNav.NavItem>
-      <TabNav.NavItem to="charts">Charts</TabNav.NavItem>
-      <TabNav.NavItem to="genres">Genres &amp; Moods</TabNav.NavItem>
-      <TabNav.NavItem to="new-releases">New Releases</TabNav.NavItem>
-      <TabNav.NavItem to="discover">Discover</TabNav.NavItem>
-    </TabNav>
+    <header>
+      <TabNav>
+        <TabNav.NavItem to="featured">Featured</TabNav.NavItem>
+        <TabNav.NavItem to="podcasts">Podcasts</TabNav.NavItem>
+        <TabNav.NavItem to="charts">Charts</TabNav.NavItem>
+        <TabNav.NavItem to="genres">Genres &amp; Moods</TabNav.NavItem>
+        <TabNav.NavItem to="new-releases">New Releases</TabNav.NavItem>
+        <TabNav.NavItem to="discover">Discover</TabNav.NavItem>
+      </TabNav>
+    </header>
     {children}
   </>
 )
