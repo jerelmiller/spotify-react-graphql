12daa760a597e190d825a41610dd5406e33f75f4
Jerel Miller <jerelmiller@gmail.com>
2018-11-27 23:13:52 -0800

    Start adding more succinct typography to theme

--- client/src/styles/theme/typography.js
+++ client/src/styles/theme/typography.js
@@ -1,8 +1,31 @@
 import colors from './colors'
 
+const size = {
+  xs: '0.8rem',
+  sm: '0.9rem'
+}
+
+const weight = {
+  light: 300
+}
+
+const textColors = {
+  primary: colors.white,
+  muted: colors.offWhite
+}
+
 export default {
-  colors: {
-    primary: colors.white,
-    muted: colors.offWhite
+  colors: textColors,
+  size,
+  weight,
+  style: {
+    xs: {
+      weight: weight.light,
+      size: size.xs
+    },
+    sm: {
+      weight: weight.light,
+      size: size.sm
+    }
   }
 }
