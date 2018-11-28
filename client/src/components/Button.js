cb3f57f2250e4330654a878bfd34745284989a28
Jerel Miller <jerelmiller@gmail.com>
2018-11-27 23:33:41 -0800

    Add some button variants

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,12 +1,39 @@
-import styled from 'styled-components'
-import { color } from 'styles/utils'
+import styled, { css } from 'styled-components'
+import { color, typography } from 'styles/utils'
+
+const SIZES = {
+  sm: css`
+    ${typography('xs')};
+    font-weight: normal;
+    letter-spacing: 1.2px;
+    padding: 0.75rem 3rem;
+  `,
+  md: css``
+}
+
+const KINDS = {
+  primary: css`
+    color: ${color('white')};
+    background: ${color('green')};
+    border-color: ${color('green')};
+  `,
+  ghost: css`
+    color: ${color('white')};
+    background: transparent;
+    border-color: ${color('offWhite')};
+  `
+}
 
 const Button = styled.button`
-  color: ${color('white')};
-  background: ${color('green')};
-  padding: 1.25rem 2rem;
   border-radius: 10rem;
   font-size: 1.25rem;
+  border: 2px solid;
+  text-transform: uppercase;
+
+  ${({ size, kind }) => css`
+    ${SIZES[size]};
+    ${KINDS[kind]};
+  `};
 `
 
 export default Button
