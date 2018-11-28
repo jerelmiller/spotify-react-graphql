23e0a24355fb8c124e603288cb0884a12f875ad8
Jerel Miller <jerelmiller@gmail.com>
2018-11-27 23:53:14 -0800

    More button styles

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,5 +1,8 @@
 import styled, { css } from 'styled-components'
-import { color, typography } from 'styles/utils'
+import { color, lighten, typography } from 'styles/utils'
+
+const GREEN = color('green')
+const LIGHT_GREEN = lighten(0.1, 'green')
 
 const SIZES = {
   sm: css`
@@ -14,26 +17,51 @@ const SIZES = {
 const KINDS = {
   primary: css`
     color: ${color('white')};
-    background: ${color('green')};
-    border-color: ${color('green')};
+    background: ${GREEN};
+    border-color: ${GREEN};
+
+    &:hover {
+      background: ${LIGHT_GREEN};
+      border-color: ${LIGHT_GREEN};
+    }
   `,
   ghost: css`
     color: ${color('white')};
     background: transparent;
     border-color: ${color('offWhite')};
+
+    &:hover {
+      border-color: ${color('white')};
+    }
   `
 }
 
-const Button = styled.button`
+const Button = styled.button.attrs({
+  className: 'sp-btn'
+})`
   border-radius: 10rem;
   font-size: 1.25rem;
   border: 2px solid;
   text-transform: uppercase;
+  transition: all 0.15s ease-in-out;
+  cursor: pointer;
+
+  &:focus {
+    outline: 0;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+  }
 
   ${({ size, kind }) => css`
     ${SIZES[size]};
     ${KINDS[kind]};
   `};
+
+  &.sp-btn + .sp-btn {
+    margin-left: 1rem;
+  }
 `
 
 export default Button
