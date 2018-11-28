8bf05a12dcd943296c56e69b80691dbdc63209c7
Jerel Miller <jerelmiller@gmail.com>
2018-11-27 23:13:52 -0800

    Start adding more succinct typography to theme

--- client/src/styles/utils.js
+++ client/src/styles/utils.js
@@ -1,4 +1,5 @@
 import { curry } from 'utils/fp'
+import { css } from 'styled-components'
 
 export const addUnits = curry(
   (unit, value) => (typeof value === 'number' ? value + unit : value)
@@ -9,3 +10,13 @@ export const color = curry((name, { theme }) => theme.colors[name])
 export const textColor = curry(
   (name, { theme }) => theme.typography.colors[name]
 )
+
+export const typography = curry((name, { theme }) => {
+  const { color, size, weight } = theme.typography.style[name] || {}
+
+  return css`
+    color: ${color};
+    font-size: ${size};
+    font-weight: ${weight};
+  `
+})
