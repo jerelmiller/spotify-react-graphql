3a1667ba3d7d01c02399ee4ea84031b4969f2470
Jerel Miller <jerelmiller@gmail.com>
2018-12-01 19:50:44 -0800

    Fetch data for artist overview

--- client/src/routes/artist/overview.js
+++ client/src/routes/artist/overview.js
@@ -1,3 +1,24 @@
-const Overview = () => 'Overview'
+import React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+const Overview = ({ artistId }) => (
+  <Query
+    query={gql`
+      query ArtistOverviewQuery($artistId: ID!, $limit: Int!) {
+        artist(id: $artistId) {
+          id
+          topTracks(limit: $limit) {
+            id
+            name
+          }
+        }
+      }
+    `}
+    variables={{ artistId, limit: 5 }}
+  >
+    {({ loading, data }) => <h1>Popular</h1>}
+  </Query>
+)
 
 export default Overview
