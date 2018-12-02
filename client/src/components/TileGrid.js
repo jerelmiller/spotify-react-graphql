314e353727f55ddb2d8d430a9e853f5b738fbf5d
Jerel Miller <jerelmiller@gmail.com>
2018-12-01 23:02:29 -0800

    Handle auto-fill for tile grid

--- client/src/components/TileGrid.js
+++ client/src/components/TileGrid.js
@@ -4,11 +4,15 @@ import { prop } from 'utils/fp'
 const TileGrid = styled.div`
   display: grid;
   grid-gap: ${prop('gap')};
-  grid-template-columns: repeat(auto-fit, minmax(${prop('minWidth')}, 1fr));
+  grid-template-columns: repeat(
+    ${({ fill }) => (fill ? 'auto-fit' : 'auto-fill')},
+    minmax(${prop('minWidth')}, 1fr)
+  );
 `
 
 TileGrid.defaultProps = {
-  gap: '2.5rem 1rem'
+  gap: '2.5rem 1rem',
+  fill: true
 }
 
 export default TileGrid
