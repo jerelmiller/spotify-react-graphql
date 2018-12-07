d15d451c5f69fdb6f3d8e2b60960416b8edd63e0
Jerel Miller <jerelmiller@gmail.com>
2018-12-07 00:23:53 -0800

    Add transition on gradient instead of removing it completely

--- client/src/components/AppMain.js
+++ client/src/components/AppMain.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import useBackgroundColor from 'hooks/useBackgroundColor'
 import { prop } from 'utils/fp'
 
@@ -28,19 +28,17 @@ const Backdrop = styled.div`
   right: 0;
   z-index: -1;
 
-  ${({ usingGradient }) =>
-    usingGradient &&
-    css`
-      &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background: linear-gradient(rgba(255, 255, 255, 0), #04060b);
-      }
-    `}
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(rgba(255, 255, 255, 0), #04060b);
+    transition: opacity 0.15s ease-in-out;
+    opacity: ${({ usingGradient }) => (usingGradient ? 1 : 0)};
+  }
 `
 
 const AppMain = ({ children }) => {
