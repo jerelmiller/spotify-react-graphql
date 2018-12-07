ca2a6ed29a8a58596b89ba7daaa055f760bc6e64
Jerel Miller <jerelmiller@gmail.com>
2018-12-06 22:33:47 -0800

    Add related artists implementation

--- client/src/routes/artist/related-artists.js
+++ client/src/routes/artist/related-artists.js
@@ -1,3 +1,35 @@
-const RelatedArtists = ({ artistId, children }) => 'related artists'
+import React from 'react'
+import ArtistTile from 'components/ArtistTile'
+import gql from 'graphql-tag'
+import TileGrid from 'components/TileGrid'
+import { Query } from 'react-apollo'
+
+const RelatedArtists = ({ artistId, children }) => (
+  <Query
+    query={gql`
+      query RelatedArtistQuery($artistId: ID!) {
+        artist(id: $artistId) {
+          id
+          relatedArtists {
+            id
+            ...Artist_artist
+          }
+        }
+      }
+
+      ${ArtistTile.fragments.artist}
+    `}
+    variables={{ artistId }}
+  >
+    {({ loading, data: { artist } }) => (
+      <TileGrid minWidth="160px">
+        {loading ||
+          artist.relatedArtists.map(artist => (
+            <ArtistTile key={artist.id} artist={artist} />
+          ))}
+      </TileGrid>
+    )}
+  </Query>
+)
 
 export default RelatedArtists
