30e8ca08bb59ac44f3512fc4fef7d89addb0e345
Jerel Miller <jerelmiller@gmail.com>
2018-12-10 02:56:58 -0800

    Move currentTime into useSpotify

--- client/src/hooks/useSpotify.js
+++ client/src/hooks/useSpotify.js
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react'
-import useScript from './useScript'
 import { compose, prop } from 'utils/fp'
+import useScript from './useScript'
+import useTimer from './useTimer'
+
+const DEFAULT_STATE = {
+  isPlayingThroughPlayer: false,
+  paused: true,
+  position: 0,
+  duration: 0
+}
 
 const useSpotify = token => {
   const [player, setPlayer] = useState(null)
-  const [state, setState] = useState(null)
+  const [state, setState] = useState(DEFAULT_STATE)
   const [error, setError] = useState(null)
   const setSpotifyError = compose(
     setError,
     prop('message')
   )
+  const [currentTime, setCurrentTime] = useState(0)
 
   const parseState = state =>
-    state && {
-      paused: state.paused,
-      position: state.position,
-      duration: state.duration
-    }
+    state
+      ? {
+          isPlayingThroughPlayer: true,
+          paused: state.paused,
+          position: state.position,
+          duration: state.duration
+        }
+      : DEFAULT_STATE
 
   const setPlayerState = compose(
     setState,
@@ -58,7 +70,28 @@ const useSpotify = token => {
   )
   useScript('https://sdk.scdn.co/spotify-player.js')
 
-  return { error, isPlayingThroughPlayer: Boolean(state), ...state }
+  useTimer(
+    () => {
+      setCurrentTime(currentTime + 1000)
+    },
+    { on: !state.paused },
+    [currentTime]
+  )
+
+  useEffect(
+    () => {
+      if (state.position != null) {
+        setCurrentTime(state.position)
+      }
+    },
+    [state.position]
+  )
+
+  return {
+    error,
+    currentTime,
+    ...state
+  }
 }
 
 export default useSpotify
