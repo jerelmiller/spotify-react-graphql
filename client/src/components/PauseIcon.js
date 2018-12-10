b24bd76d31e9c5341cd1a01030a91de7891fb0f1
Jerel Miller <jerelmiller@gmail.com>
2018-12-10 01:11:12 -0800

    Add a pause icon

---
+++ client/src/components/PauseIcon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FeatherSVG from './FeatherSVG'
+
+const PauseIcon = props => (
+  <FeatherSVG {...props}>
+    <title>pause</title>
+    <rect x="6" y="4" width="4" height="16" />
+    <rect x="14" y="4" width="4" height="16" />
+  </FeatherSVG>
+)
+
+export default PauseIcon
