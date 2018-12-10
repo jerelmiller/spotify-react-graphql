07c869309971b29b3624b5758adee456e4c58f8e
Jerel Miller <jerelmiller@gmail.com>
2018-12-09 23:23:55 -0800

    Add a useScript hook

---
+++ client/src/hooks/useScript.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+
+const useScript = src => {
+  useEffect(
+    () => {
+      const script = document.createElement('script')
+      script.setAttribute('src', src)
+      document.body.appendChild(script)
+
+      return () => document.body.removeChild(script)
+    },
+    [src]
+  )
+}
+
+export default useScript
