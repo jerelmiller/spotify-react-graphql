07ddcde169860e733dc234a66073b8f0418938b7
Jerel Miller <jerelmiller@gmail.com>
2018-12-10 23:52:58 -0800

    Render full playlist tile

--- client/src/components/PlaylistTile.js
+++ client/src/components/PlaylistTile.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import gql from 'graphql-tag'
+import LazyImage from 'components/LazyImage'
+import styled from 'styled-components'
+import { Link } from '@reach/router'
 
-const PlaylistTile = ({ playlist }) => <div>{playlist.name}</div>
+const Container = styled.div`
+  text-align: center;
+`
+
+const PlaylistTile = ({ playlist }) => {
+  const photo = playlist.images[0]
+  const href = `/playlists/${playlist.id}`
+
+  return (
+    <Container>
+      <Link to={href}>
+        <LazyImage src={photo.url} width="100%" />
+      </Link>
+      <Link to={href}>{playlist.name}</Link>
+    </Container>
+  )
+}
 
 PlaylistTile.fragments = {
   playlist: gql`
     fragment PlaylistTile_playlist on Playlist {
       id
       name
+      images {
+        url
+      }
     }
   `
 }
