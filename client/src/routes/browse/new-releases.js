e69e50ba0dafe236d684f7c07873dbc42058f14c
Jerel Miller <jerelmiller@gmail.com>
2018-12-10 22:45:37 -0800

    Add background to new releases

--- client/src/routes/browse/new-releases.js
+++ client/src/routes/browse/new-releases.js
@@ -4,46 +4,51 @@ import PageTitle from 'components/PageTitle'
 import TileGrid from 'components/TileGrid'
 import AlbumTile from 'components/AlbumTile'
 import { Query } from 'react-apollo'
+import useBackgroundColor from 'hooks/useBackgroundColor'
 
-const NewReleases = () => (
-  <Query
-    query={gql`
-      query NewReleasesQuery($limit: Int!, $offset: Int!) {
-        newReleases(limit: $limit, offset: $offset) {
-          edges {
-            node {
-              id
-              ...Album_album
+const NewReleases = () => {
+  useBackgroundColor('#283A6A')
+
+  return (
+    <Query
+      query={gql`
+        query NewReleasesQuery($limit: Int!, $offset: Int!) {
+          newReleases(limit: $limit, offset: $offset) {
+            edges {
+              node {
+                id
+                ...Album_album
+              }
+            }
+          }
+
+          featuredPlaylists {
+            edges {
+              node {
+                id
+                name
+              }
             }
           }
         }
 
-        featuredPlaylists {
-          edges {
-            node {
-              id
-              name
-            }
-          }
-        }
-      }
-
-      ${AlbumTile.fragments.album}
-    `}
-    variables={{ limit: 50, offset: 0 }}
-  >
-    {({ loading, data: { newReleases } }) => (
-      <>
-        <PageTitle>New Releases</PageTitle>
-        <TileGrid minWidth="180px">
-          {loading ||
-            newReleases.edges.map(({ node }) => (
-              <AlbumTile key={node.id} album={node} />
-            ))}
-        </TileGrid>
-      </>
-    )}
-  </Query>
-)
+        ${AlbumTile.fragments.album}
+      `}
+      variables={{ limit: 50, offset: 0 }}
+    >
+      {({ loading, data: { newReleases } }) => (
+        <>
+          <PageTitle>New Releases</PageTitle>
+          <TileGrid minWidth="180px">
+            {loading ||
+              newReleases.edges.map(({ node }) => (
+                <AlbumTile key={node.id} album={node} />
+              ))}
+          </TileGrid>
+        </>
+      )}
+    </Query>
+  )
+}
 
 export default NewReleases
