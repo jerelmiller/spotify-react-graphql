78c056fdf54d99d7c18dcfad858630f8836da37a
Jerel Miller <jerelmiller@gmail.com>
2018-12-10 23:08:07 -0800

    Remove podcasts and charts for now

--- client/src/routes/browse.js
+++ client/src/routes/browse.js
@@ -6,8 +6,6 @@ const RouteBrowse = ({ children }) => (
     <header>
       <TabNav>
         <TabNav.NavItem to="featured">Featured</TabNav.NavItem>
-        <TabNav.NavItem to="podcasts">Podcasts</TabNav.NavItem>
-        <TabNav.NavItem to="charts">Charts</TabNav.NavItem>
         <TabNav.NavItem to="genres">Genres &amp; Moods</TabNav.NavItem>
         <TabNav.NavItem to="new-releases">New Releases</TabNav.NavItem>
         <TabNav.NavItem to="discover">Discover</TabNav.NavItem>
