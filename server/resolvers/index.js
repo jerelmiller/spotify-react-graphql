67726c8f52b6d27e0ca8ecd829758441d0c7b377
Jerel Miller <jerelmiller@gmail.com>
2019-01-15 21:38:16 -0800

    More automation when adding new resolvers

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,61 +1,22 @@
-import Album from './Album'
-import AlbumConnection from './AlbumConnection'
-import AlbumEdge from './AlbumEdge'
-import Artist from './Artist'
-import ArtistConnection from './ArtistConnection'
-import ArtistEdge from './ArtistEdge'
-import CategoryConnection from './CategoryConnection'
-import CategoryEdge from './CategoryEdge'
-import CursorInfo from './CursorInfo'
-import FullTrack from './FullTrack'
-import Mutation from './Mutation'
-import Playlist from './Playlist'
-import PlaylistConnection from './PlaylistConnection'
-import PlaylistEdge from './PlaylistEdge'
-import PlaylistTrackConnection from './PlaylistTrackConnection'
-import PlaylistTrackEdge from './PlaylistTrackEdge'
-import Query from './Query'
-import SavedAlbumConnection from './SavedAlbumConnection'
-import SavedAlbumEdge from './SavedAlbumEdge'
-import SavedTrack from './SavedTrack'
-import SavedTrackConnection from './SavedTrackConnection'
-import SavedTrackEdge from './SavedTrackEdge'
-import SimpleTrack from './SimpleTrack'
-import Track from './Track'
-import TrackConnection from './TrackConnection'
-import TrackEdge from './TrackEdge'
-import User from './User'
-import Viewer from './Viewer'
+import fs from 'fs'
+import path from 'path'
 
-const resolvers = {
-  Query,
-  Mutation,
-  Album,
-  AlbumConnection,
-  AlbumEdge,
-  Artist,
-  ArtistConnection,
-  ArtistEdge,
-  CategoryConnection,
-  CategoryEdge,
-  CursorInfo,
-  FullTrack,
-  Playlist,
-  PlaylistConnection,
-  PlaylistEdge,
-  PlaylistTrackConnection,
-  PlaylistTrackEdge,
-  SavedAlbumConnection,
-  SavedAlbumEdge,
-  SavedTrack,
-  SavedTrackConnection,
-  SavedTrackEdge,
-  SimpleTrack,
-  Track,
-  TrackConnection,
-  TrackEdge,
-  User,
-  Viewer
-}
+const currentFilename = path.basename(__filename)
+const isJSFile = filename => /\.js$/.test(filename)
+
+// automatically require all files in current directory
+const resolvers = fs
+  .readdirSync(path.resolve(__dirname))
+  .filter(filename => isJSFile(filename) && filename !== currentFilename)
+  .reduce(
+    (resolvers, filename) => ({
+      ...resolvers,
+      [path.basename(filename, '.js')]: require(path.resolve(
+        __dirname,
+        filename
+      )).default
+    }),
+    {}
+  )
 
 export default resolvers
