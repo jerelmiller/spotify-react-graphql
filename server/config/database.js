b00fd3553b091d7ebba63d61b6c2ee8f287011f7
Jerel Miller <jerelmiller@gmail.com>
2019-01-16 01:23:53 -0800

    Better db setup

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,23 @@
-import Sequelize from 'sequelize'
-
-export const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'spotify_development',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'spotify_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'spotify',
+    host: '127.0.0.1',
     dialect: 'postgres'
   }
-)
-
-export { Sequelize }
+}
