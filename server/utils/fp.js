29087e40a753580423881d287a1fb8c01abaf0cc
Jerel Miller <jerelmiller@gmail.com>
2019-01-15 22:58:28 -0800

    Add a filter keys and filterNullValues helper

--- server/utils/fp.js
+++ server/utils/fp.js
@@ -1,2 +1,8 @@
 export const prop = name => obj => obj[name]
 export const self = item => item
+export const filterKeys = fn => obj =>
+  Object.entries(obj)
+    .filter(fn)
+    .reduce((memo, [key, value]) => ({ ...memo, [key]: value }))
+
+export const filterNullValues = filterKeys(([_, value]) => Boolean(value))
