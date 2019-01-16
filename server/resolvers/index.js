b3862e40d87c1a58359dccc9d49f4644ce0900f4
Jerel Miller <jerelmiller@gmail.com>
2019-01-16 01:03:35 -0800

    Use helper to require resolvers

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,22 +1,14 @@
 import fs from 'fs'
 import path from 'path'
+import { getRelativeFiles } from '../utils/fs'
 
-const currentFilename = path.basename(__filename)
-const isJSFile = filename => /\.js$/.test(filename)
-
-// automatically require all files in current directory
-const resolvers = fs
-  .readdirSync(path.resolve(__dirname))
-  .filter(filename => isJSFile(filename) && filename !== currentFilename)
-  .reduce(
-    (resolvers, filename) => ({
-      ...resolvers,
-      [path.basename(filename, '.js')]: require(path.resolve(
-        __dirname,
-        filename
-      )).default
-    }),
-    {}
-  )
+const resolvers = getRelativeFiles(__dirname).reduce(
+  (resolvers, filename) => ({
+    ...resolvers,
+    [path.basename(filename, '.js')]: require(path.resolve(__dirname, filename))
+      .default
+  }),
+  {}
+)
 
 export default resolvers
