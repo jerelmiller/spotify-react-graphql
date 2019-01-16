400fefa0cf239fb139ae705d0b3be4e9718f1f3c
Jerel Miller <jerelmiller@gmail.com>
2019-01-16 01:48:37 -0800

    Add other fields needed for session

--- server/migrations/20190116085524-create-session.js
+++ server/migrations/20190116085524-create-session.js
@@ -1,25 +1,45 @@
 'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('sessions', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      refreshToken: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    })
+    return queryInterface
+      .createTable('sessions', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        accessToken: {
+          allowNull: false,
+          type: Sequelize.TEXT
+        },
+        refreshToken: {
+          allowNull: false,
+          type: Sequelize.TEXT
+        },
+        expiresAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        scopes: {
+          allowNull: false,
+          type: Sequelize.TEXT,
+          defaultValue: ''
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      })
+      .then(() =>
+        queryInterface.addConstraint('sessions', ['accessToken'], {
+          type: 'unique'
+        })
+      )
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('sessions')
