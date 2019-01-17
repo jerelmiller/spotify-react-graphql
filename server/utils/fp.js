66fdc3d1eae66a9fea911359e309e850812564e8
Jerel Miller <jerelmiller@gmail.com>
2019-01-16 17:45:24 -0800

    Add omit helper to server utils

--- server/utils/fp.js
+++ server/utils/fp.js
@@ -1,8 +1,25 @@
+export const curry = fn => {
+  const curried = (...args) => {
+    if (args.length >= fn.length) {
+      return fn(...args)
+    }
+
+    return (...args2) => curried(...args.concat(args2))
+  }
+
+  return curried
+}
+
 export const prop = name => obj => obj[name]
 export const self = item => item
-export const filterKeys = fn => obj =>
+export const filterKeys = curry((fn, obj) =>
   Object.entries(obj)
     .filter(fn)
     .reduce((memo, [key, value]) => ({ ...memo, [key]: value }), {})
+)
 
 export const filterNullValues = filterKeys(([_, value]) => Boolean(value))
+
+export const omit = curry((keys, obj) =>
+  filterKeys(([key]) => !keys.includes(key), obj)
+)
