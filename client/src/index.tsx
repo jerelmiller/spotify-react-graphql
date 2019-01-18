7be39ab4abe5ae77b18cb7a3f5928ff695d2725d
Jerel Miller <jerelmiller@gmail.com>
2019-01-17 22:09:52 -0800

    use * import for react

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import App from './App'
 import BackgroundColorProvider from './components/BackgroundColorProvider'
 import client from './config/apollo'
