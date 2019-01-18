c59fd060d02c2e27a743b51f24a67a542638c7ea
Jerel Miller <jerelmiller@gmail.com>
2019-01-17 22:31:02 -0800

    Update typography to be typescript

--- client/src/components/Typography.tsx
+++ client/src/components/Typography.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-import { color, typography } from 'styles/utils'
-import { compose, prop } from 'utils/fp'
+import { color, typography } from '../styles/utils'
+import { compose, prop } from '../utils/fp'
 
-const Typography = styled.span`
+const Typography = styled('span')<{ kind: string }>`
   ${({ kind }) => typography(kind)};
   color: ${compose(
     color,
