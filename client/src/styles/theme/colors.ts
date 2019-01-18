6f202bc936afacd65af9b4a72373b5197d165666
Jerel Miller <jerelmiller@gmail.com>
2019-01-17 21:38:54 -0800

    Add types for theme

---
+++ client/src/styles/theme/colors.ts
@@ -0,0 +1,13 @@
+export type Hex = string
+
+export interface Colors {
+  green: Hex
+  offWhite: Hex
+  white: Hex
+}
+
+export const colors = {
+  green: '#2ab759',
+  offWhite: '#B3B3B3',
+  white: '#fff'
+}
---
+++ client/src/styles/theme/typography.ts
@@ -0,0 +1,76 @@
+import colors, { Hex } from './colors'
+
+enum WeightValue {
+  light = 300,
+  normal = 400
+}
+
+interface Weight {
+  light: WeightValue.light
+  normal: WeightValue.normal
+}
+
+interface Size {
+  xs: string
+  sm: string
+  normal: string
+}
+
+interface TextColors {
+  primary: Hex
+  muted: Hex
+  highlight: Hex
+}
+
+interface TypographyPreset {
+  size: string
+  weight: WeightValue
+}
+
+interface TypographyStyle {
+  xs: TypographyPreset
+  sm: TypographyPreset
+  md: TypographyPreset
+}
+
+export interface Typography {
+  colors: TextColors
+  size: Size
+  weight: Weight
+  style: TypographyStyle
+}
+
+const size = {
+  xs: '0.8rem',
+  sm: '0.9rem',
+  normal: '1rem'
+}
+
+const weight = {
+  light: 300,
+  normal: 400
+}
+
+export const typography: Typography = {
+  colors: {
+    primary: colors.white,
+    muted: colors.offWhite,
+    highlight: colors.green
+  },
+  size,
+  weight,
+  style: {
+    xs: {
+      weight: weight.light,
+      size: size.xs
+    },
+    sm: {
+      weight: weight.light,
+      size: size.sm
+    },
+    md: {
+      weight: weight.normal,
+      size: size.normal
+    }
+  }
+}
