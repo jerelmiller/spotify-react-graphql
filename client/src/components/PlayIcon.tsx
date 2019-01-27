dc4aacdabbd2db0dd1d31fa58b0f66d6c6942466
Jerel Miller <jerelmiller@gmail.com>
2019-01-27 03:07:27 -0800

    Update PlayIcon to ts

---
+++ client/src/components/PlayIcon.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import FeatherSVG, { Props as FeatherSVGProps } from './FeatherSVG'
+
+interface Props {}
+
+const PlayIcon: FC<Props & FeatherSVGProps> = props => (
+  <FeatherSVG {...props}>
+    <title>play</title>
+    <polygon points="5 3 19 12 5 21 5 3" />
+  </FeatherSVG>
+)
+
+export default PlayIcon
