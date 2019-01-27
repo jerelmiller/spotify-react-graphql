7d2bdf594080c11d00d070ca1d4b6052938a5f2a
Jerel Miller <jerelmiller@gmail.com>
2019-01-27 00:58:06 -0800

    Rename query to RouteAlbumsQuery

--- client/src/routes/collection/types/RouteAlbumsQuery.ts
+++ client/src/routes/collection/types/RouteAlbumsQuery.ts
@@ -2,16 +2,16 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL query operation: AlbumsQuery
+// GraphQL query operation: RouteAlbumsQuery
 // ====================================================
 
-export interface AlbumsQuery_viewer_savedAlbums_edges_node_artists {
+export interface RouteAlbumsQuery_viewer_savedAlbums_edges_node_artists {
   __typename: "SimpleArtist";
   id: string;
   name: string;
 }
 
-export interface AlbumsQuery_viewer_savedAlbums_edges_node_images {
+export interface RouteAlbumsQuery_viewer_savedAlbums_edges_node_images {
   __typename: "Image";
   /**
    * The source URL of the image.
@@ -19,7 +19,7 @@ export interface AlbumsQuery_viewer_savedAlbums_edges_node_images {
   url: string;
 }
 
-export interface AlbumsQuery_viewer_savedAlbums_edges_node {
+export interface RouteAlbumsQuery_viewer_savedAlbums_edges_node {
   __typename: "Album";
   id: string;
   /**
@@ -30,42 +30,42 @@ export interface AlbumsQuery_viewer_savedAlbums_edges_node {
   /**
    * The artists of the album.
    */
-  artists: AlbumsQuery_viewer_savedAlbums_edges_node_artists[];
+  artists: RouteAlbumsQuery_viewer_savedAlbums_edges_node_artists[];
   /**
    * The cover art for the album in various sizes, widest first.
    */
-  images: AlbumsQuery_viewer_savedAlbums_edges_node_images[];
+  images: RouteAlbumsQuery_viewer_savedAlbums_edges_node_images[];
 }
 
-export interface AlbumsQuery_viewer_savedAlbums_edges {
+export interface RouteAlbumsQuery_viewer_savedAlbums_edges {
   __typename: "SavedAlbumEdge";
   /**
    * The album object.
    */
-  node: AlbumsQuery_viewer_savedAlbums_edges_node;
+  node: RouteAlbumsQuery_viewer_savedAlbums_edges_node;
 }
 
-export interface AlbumsQuery_viewer_savedAlbums {
+export interface RouteAlbumsQuery_viewer_savedAlbums {
   __typename: "SavedAlbumConnection";
-  edges: AlbumsQuery_viewer_savedAlbums_edges[];
+  edges: RouteAlbumsQuery_viewer_savedAlbums_edges[];
 }
 
-export interface AlbumsQuery_viewer {
+export interface RouteAlbumsQuery_viewer {
   __typename: "Viewer";
   /**
    * The collection of saved albums in the current user's Spotify library.
    */
-  savedAlbums: AlbumsQuery_viewer_savedAlbums | null;
+  savedAlbums: RouteAlbumsQuery_viewer_savedAlbums | null;
 }
 
-export interface AlbumsQuery {
+export interface RouteAlbumsQuery {
   /**
    * Get information for the current logged-in user
    */
-  viewer: AlbumsQuery_viewer | null;
+  viewer: RouteAlbumsQuery_viewer | null;
 }
 
-export interface AlbumsQueryVariables {
+export interface RouteAlbumsQueryVariables {
   limit: number;
   offset: number;
 }
