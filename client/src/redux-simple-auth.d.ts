7151c37814be1d2a02b5b0f0aba85c33200e48a6
Jerel Miller <jerelmiller@gmail.com>
2019-01-27 20:53:41 -0800

    Convert authenticator to ts. Update store to use generic

--- client/src/redux-simple-auth.d.ts
+++ client/src/redux-simple-auth.d.ts
@@ -1,7 +1,17 @@
 declare module 'redux-simple-auth' {
   export interface MiddlewareOptions<Data> {
-    authenticators: any[]
+    authenticators: Authenticator[]
     authorize(data: Data, header: (name: string, value: string) => void): void
+  }
+
+  export interface Authenticator<Data> {
+    restore(data: Data): Promise<Data>
+  }
+
+  export interface AuthenticatorOptions<Data> {
+    name: string
+    authenticate(data: any): Promise<Data>
+    restore(data: any): Promise<any>
   }
 
   export function getSessionData(state: Object): any
@@ -10,4 +20,8 @@ declare module 'redux-simple-auth' {
   export function createAuthMiddleware<Data>(
     options: MiddlewareOptions<Data>
   ): Redux.Middleware
+
+  export function createAuthenticator<Data>(
+    options: AuthenticatorOptions<Data>
+  ): Authenticator
 }
