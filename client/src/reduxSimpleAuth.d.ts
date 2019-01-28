b7e1a4383af51a669b6229ee86fde665241fd36c
Jerel Miller <jerelmiller@gmail.com>
2019-01-27 20:46:23 -0800

    Rename redux-simple-auth types

--- client/src/reduxSimpleAuth.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'redux-simple-auth' {
-  export function getSessionData(state: Object): any
-  export function invalidateSession(): Promise<any>
-  export function authenticate(name: string, data: object): Promise<any>
-}
