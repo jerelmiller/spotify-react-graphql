ed7aa797e614ca2b0cb583bdd8f30eec3958367a
Jerel Miller <jerelmiller@gmail.com>
2019-01-27 21:01:11 -0800

    Convert QueryParams to ts

--- client/src/components/QueryParams.tsx
+++ client/src/components/QueryParams.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
 import { Location } from '@reach/router'
 
-const QueryParams = ({ children }) => (
+interface Props {
+  children(params: URLSearchParams): ReactNode
+}
+
+const QueryParams: FC<Props> = ({ children }) => (
   <Location>
     {({ location }) => children(new URLSearchParams(location.search))}
   </Location>
