2a3369414ce5c5b648cb9a09fedc9c965278c4e4
Jerel Miller <jerelmiller@gmail.com>
2019-02-18 19:41:03 -0800

    Use ramda for some functions

--- client/src/components/LazyBackgroundImage.js
+++ client/src/components/LazyBackgroundImage.js
@@ -1,12 +1,12 @@
 import React, { forwardRef } from 'react'
 import styled from 'styled-components'
 import useLazyImage from 'hooks/useLazyImage'
-import { branch, prop, value } from 'utils/fp'
+import { ifElse, prop, value } from 'utils/fp'
 
 const BackgroundImage = styled.div`
   background-image: url(${prop('src')});
   transition: opacity 0.3s ease-out;
-  opacity: ${branch(prop('loaded'), value(1), value(0))};
+  opacity: ${ifElse(prop('loaded'), value(1), value(0))};
 `
 
 const LazyBackgroundImage = ({ component, src, ...props }, ref) => {
--- client/src/utils/fp.js
+++ client/src/utils/fp.js
@@ -1,23 +1,4 @@
-export const curry = fn => {
-  const curried = (...args) => {
-    if (args.length >= fn.length) {
-      return fn(...args)
-    }
-
-    return (...args2) => curried(...args.concat(args2))
-  }
-
-  return curried
-}
-
-export const compose = (...fns) =>
-  fns.reduce((f, g) => (...args) => f(g(...args)))
-
-export const branch = curry((predicate, ifTrue, ifFalse, obj) =>
-  predicate(obj) ? ifTrue(obj) : ifFalse()
-)
-
-export const defaultTo = curry((def, obj) => (obj == null ? def : obj))
 export const noop = () => {}
-export const prop = curry((name, obj) => obj[name])
 export const value = val => () => val
+
+export { curry, ifElse, compose, defaultTo, prop } from 'ramda'
