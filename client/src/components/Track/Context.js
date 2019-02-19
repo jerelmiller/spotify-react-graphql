5e6625ec7f9165bdf98902040f92abf0c17bbdc8
Jerel Miller <jerelmiller@gmail.com>
2019-02-18 23:41:52 -0800

    Convert TrackContext to ts

--- client/src/components/Track/Context.js
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react'
-
-const TrackContext = createContext()
-
-export const useTrackContext = () => useContext(TrackContext)
-export default TrackContext
---
+++ client/src/components/Track/Context.ts
@@ -0,0 +1,21 @@
+import { createContext, useContext } from 'react'
+import { Track_track } from './types/Track_track'
+
+interface PlayTrackOptions {
+  context?: string
+}
+
+interface Context {
+  hovered: boolean
+  track: Track_track | null
+  playTrack(uri: string, options: PlayTrackOptions): Promise<any>
+}
+
+const TrackContext = createContext<Context>({
+  hovered: false,
+  playTrack: () => Promise.reject(),
+  track: null
+})
+
+export const useTrackContext = () => useContext(TrackContext)
+export default TrackContext
