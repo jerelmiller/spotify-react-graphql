2c3a73ef60a7722068c8e3fc26c7e6c9b5bdc64a
Jerel Miller <jerelmiller@gmail.com>
2019-02-19 01:16:30 -0800

    Convert FeatherSVG to ts

--- client/src/components/FeatherSVG.tsx
+++ client/src/components/FeatherSVG.tsx
@@ -1,13 +1,20 @@
 import styled from 'styled-components'
-import { ifElse, compose, defaultTo, prop, value } from 'utils/fp'
-import { color } from 'styles/utils'
+import { ifElse, compose, defaultTo, prop, value } from '../utils/fp'
+import { color } from '../styles/utils'
 
-const withColor = colorValue => color(colorValue) || colorValue
+interface Props {
+  fill?: string
+  stroke?: string
+  strokeWidth?: number
+  size?: string
+}
+
+const withColor = (colorValue: string) => color(colorValue) || colorValue
 
 const FeatherSVG = styled.svg.attrs({
   viewBox: '0 0 24 24',
   xmlns: 'http://www.w3.org/2000/svg'
-})`
+})<Props>`
   fill: ${ifElse(
     prop('fill'),
     compose(
