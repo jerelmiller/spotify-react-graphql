a6c8d8a0ff343bdda4e539b0184adcc4260f1ff1
Jerel Miller <jerelmiller@gmail.com>
2019-02-18 17:59:55 -0800

    Convert remaining hooks to ts

--- client/src/hooks/useSpotifyContext.ts
+++ client/src/hooks/useSpotifyContext.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import SpotifyContext from 'components/SpotifyContext'
+import SpotifyContext from '../components/SpotifyContext'
 
 const useSpotifyContext = () => useContext(SpotifyContext)
 
