ecb4b6e1b625f225e61a7048e17d3dd65cc70e59
Jerel Miller <jerelmiller@gmail.com>
2019-02-19 01:24:29 -0800

    Define an IconComponent to better create icon components

--- client/src/components/MoreIcon.tsx
+++ client/src/components/MoreIcon.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import FeatherSVG, { Props as FeatherSVGProps } from './FeatherSVG'
-import { FC } from 'react'
+import FeatherSVG from './FeatherSVG'
+import { IconComponent } from './types/IconComponent'
 
-interface OwnProps {}
+interface Props {}
 
-type Props = OwnProps & FeatherSVGProps
-
-const MoreIcon: FC<Props> = props => (
+const MoreIcon: IconComponent<Props> = props => (
   <FeatherSVG {...props}>
     <circle cx="12" cy="12" r="1" />
     <circle cx="19" cy="12" r="1" />
