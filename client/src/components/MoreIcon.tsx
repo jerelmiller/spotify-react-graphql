f28992d62e0daefb8879f0fefb99432921cc70fd
Jerel Miller <jerelmiller@gmail.com>
2019-02-19 01:18:33 -0800

    Convert MoreIcon to ts

--- client/src/components/MoreIcon.tsx
+++ client/src/components/MoreIcon.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import FeatherSVG from './FeatherSVG'
+import FeatherSVG, { Props as FeatherSVGProps } from './FeatherSVG'
+import { FC } from 'react'
 
-const MoreIcon = props => (
+interface Props {}
+
+const MoreIcon: FC<Props & FeatherSVGProps> = props => (
   <FeatherSVG {...props}>
     <circle cx="12" cy="12" r="1" />
     <circle cx="19" cy="12" r="1" />
