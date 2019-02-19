a1d6839fbf6fa4a7c48ebd84873b1ba98ef58b68
Jerel Miller <jerelmiller@gmail.com>
2019-02-19 01:10:35 -0800

    Convert UserAvatar to ts

--- client/src/components/UserAvatar.tsx
+++ client/src/components/UserAvatar.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Avatar from './Avatar'
 import gql from 'graphql-tag'
 import styled from 'styled-components'
+import { UserAvatar_user } from './types/UserAvatar_user'
+import { FragmentComponent, GQLFragment } from '../types/shared'
+
+interface Props {
+  user: UserAvatar_user
+}
 
 const Container = styled.div`
   display: flex;
@@ -11,7 +17,9 @@ const Container = styled.div`
     margin-right: 0.5rem;
   }
 `
-const UserAvatar = ({ user = { images: [{ url: '' }] } }) => (
+const UserAvatar: FragmentComponent<Props, { user: GQLFragment }> = ({
+  user = { displayName: '', images: [{ url: '' }] }
+}) => (
   <Container>
     <Avatar image={user.images[0]} />
     <span>{user.displayName}</span>
