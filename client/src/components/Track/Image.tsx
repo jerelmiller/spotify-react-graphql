4766233871ced1086cbe01079e0b75f5975c4fbe
Jerel Miller <jerelmiller@gmail.com>
2019-03-06 23:22:38 -0800

    Add placeholder photo for track image

--- client/src/components/Track/Image.tsx
+++ client/src/components/Track/Image.tsx
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 import LazyImage from '../LazyImage'
 import { useTrackContext } from './Context'
 import { FragmentComponent, GQLFragment } from '../../types/shared'
+import PlaceholderPhoto from '../PlaceholderPhoto'
 
 interface Props {
   size: string
@@ -12,7 +13,9 @@ const Image: FragmentComponent<Props, { track: GQLFragment }> = ({ size }) => {
   const { track } = useTrackContext()
   const album = track.album.images[1]
 
-  return <LazyImage src={album.url} width={size} />
+  return (
+    <LazyImage src={album.url} width={size} fallback={<PlaceholderPhoto />} />
+  )
 }
 
 Image.fragments = {
