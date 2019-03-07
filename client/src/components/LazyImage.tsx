a155bf6e6088a7806e61612f94cb680b3d8fc40b
Jerel Miller <jerelmiller@gmail.com>
2019-03-06 21:09:50 -0800

    Add ability to specify fallback for LazyImage

--- client/src/components/LazyImage.tsx
+++ client/src/components/LazyImage.tsx
@@ -1,39 +1,65 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, ReactNode } from 'react'
 import styled from 'styled-components'
 import { ifElse, compose, defaultTo, prop, noop, value } from '../utils/fp'
 import useLazyImage from '../hooks/useLazyImage'
 import { Omit } from '../types/shared'
+import posed, { PoseGroup } from 'react-pose'
 
 interface LazyImageProps {
   src: string
-}
-
-interface ImgProps {
-  loaded: boolean
   block?: boolean
+  fallback?: ReactNode
   width?: string
 }
 
-const Img = styled.img<ImgProps>`
+type ImgProps = Pick<LazyImageProps, 'block' | 'width'>
+
+const Img = styled(
+  posed.img({
+    enter: { opacity: 1 },
+    exit: { opacity: 0 }
+  })
+)<ImgProps>`
   display: ${ifElse(prop('block'), value('block'), noop)};
   width: ${compose(
     defaultTo('100%'),
     prop('width')
   )};
-  height: ${ifElse(prop('loaded'), value('auto'), value(0))};
-  opacity: ${ifElse(prop('loaded'), value(1), value(0))};
-  padding-bottom: ${ifElse(prop('loaded'), noop, value('100%'))};
+  height: auto;
   transition: opacity 0.3s ease-out;
   object-fit: cover;
 `
 
-type Props = LazyImageProps & Omit<ImgProps, 'loaded'>
+type FallbackContainerProps = Pick<LazyImageProps, 'width'>
 
-const LazyImage = forwardRef<HTMLImageElement, Props>(
-  ({ src, ...props }, ref) => {
+const FallbackContainer = styled(
+  posed.div({
+    enter: { opacity: 1 },
+    exit: { opacity: 0 }
+  })
+)<FallbackContainerProps>`
+  padding-bottom: ${({ children }) => (children ? null : '100%')};
+  width: ${compose(
+    defaultTo('100%'),
+    prop('width')
+  )};
+`
+
+const LazyImage = forwardRef<HTMLImageElement, LazyImageProps>(
+  ({ src, width, fallback, ...props }, ref) => {
     const loaded = useLazyImage(src)
 
-    return <Img loaded={loaded} src={src} ref={ref} {...props} />
+    return (
+      <PoseGroup>
+        {loaded ? (
+          <Img key="img" src={src} width={width} ref={ref} {...props} />
+        ) : (
+          <FallbackContainer key="fallback" {...props}>
+            {fallback}
+          </FallbackContainer>
+        )}
+      </PoseGroup>
+    )
   }
 )
 
