5b1d096eeb1b9f764551bea4d19594d4b90b07a9
Jerel Miller <jerelmiller@gmail.com>
2019-03-07 21:50:17 -0800

    Fix issue with some playlists not returning tracks

--- server/resolvers/PlaylistTrackConnection.js
+++ server/resolvers/PlaylistTrackConnection.js
@@ -1,3 +1,9 @@
 import { createConnectionResolver } from './utils'
+import { prop, compose } from '../utils/fp'
 
-export default createConnectionResolver()
+export default createConnectionResolver(
+  compose(
+    Boolean,
+    prop('track')
+  )
+)
--- server/resolvers/utils/createConnectionResolver.js
+++ server/resolvers/utils/createConnectionResolver.js
@@ -1,7 +1,10 @@
-import { prop } from '../../utils/fp'
+import { filter, prop, compose } from '../../utils/fp'
 
-export default () => ({
-  edges: prop('items'),
+export default (filterFn = Boolean) => ({
+  edges: compose(
+    filter(filterFn),
+    prop('items')
+  ),
   pageInfo: ({ limit, next, offset, previous, total }) => ({
     hasNextPage: Boolean(next),
     hasPreviousPage: Boolean(previous),
