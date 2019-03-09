973a60681aafd72f74bd757611ddcd172f4cde7c
Jerel Miller <jerelmiller@gmail.com>
2019-03-09 00:51:41 -0800

    Update PlayButton to be the green play button

--- client/src/components/PlayButton.tsx
+++ client/src/components/PlayButton.tsx
@@ -1,51 +1,16 @@
 import React, { FC } from 'react'
-import PlayIcon from './PlayIcon'
-import styled from 'styled-components'
-import { prop } from '../utils/fp'
-import { MouseEvent } from 'react'
-import { rgba } from 'polished'
-import PauseIcon from './PauseIcon'
+import Button, { Props as ButtonProps } from './Button'
 
-interface Props {
-  size: string
+interface OwnProps {
   playing: boolean
-  onClick?(e: MouseEvent): void
 }
 
-const Container = styled.div<{ size: string }>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: ${prop('size')};
-  height: ${prop('size')};
-  border-radius: 50%;
-  border: 2px solid ${rgba('#fff', 0.5)};
-  transition: 0.1s ease-in-out;
-  background: none;
-  padding: 0;
-  backface-visibility: hidden;
-  transform: translateZ(0) scale(1, 1);
-  cursor: pointer;
+export type Props = OwnProps & Pick<ButtonProps, 'size' | 'onClick'>
 
-  &:hover {
-    transform: scale(1.1, 1.1);
-    border-color: #fff;
-  }
-`
-
-const CenteredPlayIcon = styled(PlayIcon)`
-  position: relative;
-  left: 5%;
-`
-
-const PlayButton: FC<Props> = ({ size, onClick, playing }) => (
-  <Container size={size} onClick={onClick}>
-    {playing ? (
-      <PauseIcon size="60%" fill="white" strokeWidth={0} />
-    ) : (
-      <CenteredPlayIcon size="60%" fill="white" />
-    )}
-  </Container>
+const PlayButton: FC<Props> = ({ playing, onClick, size }) => (
+  <Button size={size} kind="primary" onClick={onClick}>
+    {playing ? 'Pause' : 'Play'}
+  </Button>
 )
 
 export default PlayButton
