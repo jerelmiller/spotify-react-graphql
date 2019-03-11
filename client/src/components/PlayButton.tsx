a066f35297d46213088632f83f5e06c0a9b35164
Jerel Miller <jerelmiller@gmail.com>
2019-03-10 19:01:42 -0700

    Use emotion for several more compoennts

--- client/src/components/PlayButton.tsx
+++ client/src/components/PlayButton.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react'
 import Button, { Props as ButtonProps } from './Button'
+import { Omit } from '../types/shared'
 
 interface OwnProps {
   playing: boolean
 }
 
-export type Props = OwnProps & Pick<ButtonProps, 'size' | 'onClick'>
+export type Props = OwnProps & Omit<ButtonProps, 'kind'>
 
-const PlayButton: FC<Props> = ({ playing, onClick, size }) => (
-  <Button size={size} kind="primary" onClick={onClick}>
+const PlayButton: FC<Props> = ({ playing, ...props }) => (
+  <Button kind="primary" {...props}>
     {playing ? 'Pause' : 'Play'}
   </Button>
 )
