684ab7afc4467fe07cf2411feba88db3892d4bc4
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 00:16:51 -0700

    Create a useOuterClick component

---
+++ client/src/hooks/useOuterClick.ts
@@ -0,0 +1,20 @@
+import { RefObject, useEffect } from 'react'
+
+const useOuterClick = (
+  ref: RefObject<HTMLElement>,
+  onOuterClick: (e: MouseEvent) => void
+) => {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
+        onOuterClick(e)
+      }
+    }
+
+    document.addEventListener('click', handleClick)
+
+    return () => document.removeEventListener('click', handleClick)
+  }, [onOuterClick])
+}
+
+export default useOuterClick
