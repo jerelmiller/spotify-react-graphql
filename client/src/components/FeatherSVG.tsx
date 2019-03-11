ef957ef0985d8cb54a9c761930971c662406f27f
Jerel Miller <jerelmiller@gmail.com>
2019-03-10 20:28:39 -0700

    Update FeatherSVG to use emotion. Update to use styled prop

--- client/src/components/FeatherSVG.tsx
+++ client/src/components/FeatherSVG.tsx
@@ -1,47 +1,40 @@
-import styled from 'styled-components'
-import { ifElse, compose, defaultTo, prop, value } from '../utils/fp'
+import { css } from '../styled'
 import { color } from '../styles/utils'
-import { CSSProperties, HTMLAttributes } from 'react'
+import { CSSProperties, HTMLAttributes, FC } from 'react'
+import { ColorName, Theme } from '../styles/theme'
 
 export interface Props extends HTMLAttributes<SVGElement> {
   cursor?: CSSProperties['cursor']
-  fill?: CSSProperties['fill']
-  stroke?: CSSProperties['stroke']
+  fill?: ColorName
+  stroke?: ColorName
   strokeWidth?: CSSProperties['strokeWidth']
   size?: string
 }
 
-const withColor = (colorValue: string) => color(colorValue) || colorValue
-
-const FeatherSVG = styled.svg.attrs({
-  viewBox: '0 0 24 24',
-  xmlns: 'http://www.w3.org/2000/svg'
-})<Props>`
-  cursor: ${prop('cursor')};
-  fill: ${ifElse(
-    prop('fill'),
-    compose(
-      withColor,
-      prop('fill')
-    ),
-    value('none')
-  )};
-  stroke: ${ifElse(
-    prop('stroke'),
-    compose(
-      withColor,
-      prop('stroke')
-    ),
-    value('currentColor')
-  )};
-  stroke-width: ${compose(
-    defaultTo(2),
-    prop('strokeWidth')
-  )};
-  stroke-linecap: round;
-  stroke-linejoin: round;
-  width: ${prop('size')};
-  height: ${prop('size')};
-`
+const FeatherSVG: FC<Props> = ({
+  cursor,
+  fill,
+  children,
+  size,
+  stroke,
+  strokeWidth
+}) => (
+  <svg
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    css={(theme: Theme) => css`
+      cursor: ${cursor};
+      fill: ${fill ? color(fill, { theme }) : 'none'};
+      stroke: ${stroke ? color(stroke, { theme }) : 'currentColor'};
+      stroke-width: ${strokeWidth || 2};
+      stroke-linecap: round;
+      stroke-linejoin: round;
+      width: ${size};
+      height: ${size};
+    `}
+  >
+    {children}
+  </svg>
+)
 
 export default FeatherSVG
