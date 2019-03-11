7723a4fd6ad81306a3e2c0d35f3a371fbec0ec5b
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 00:44:37 -0700

    Close dropdown menu when items are clicked

--- client/src/components/DropdownMenu/Item.tsx
+++ client/src/components/DropdownMenu/Item.tsx
@@ -1,23 +1,37 @@
-import { ReactNode } from 'react'
-import styled from '../../styled'
+import React, { FC, ReactNode, useContext, MouseEvent } from 'react'
+import DropdownMenuContext from './Context'
+import { css } from '../../styled'
 import { textColor, lighten, fontSize } from '../../styles/utils'
+import { composeHandlers } from '../../utils/events'
 
 interface Props {
+  onClick?(e: MouseEvent): void
   children: ReactNode
 }
 
-const Item = styled.li<Props>`
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-  color: ${textColor('muted')};
-  white-space: nowrap;
+const Item: FC<Props> = ({ onClick, children }) => {
+  const { close } = useContext(DropdownMenuContext)
 
-  ${fontSize('sm')};
+  return (
+    <li
+      onClick={composeHandlers(onClick, close)}
+      css={theme => css`
+        padding: 0.5rem 1rem;
+        cursor: pointer;
+        color: ${textColor('muted', { theme })};
+        white-space: nowrap;
 
-  &:hover {
-    color: ${textColor('primary')};
-    background: ${lighten(0.08, 'grey')};
-  }
-`
+        ${fontSize('sm', { theme })};
+
+        &:hover {
+          color: ${textColor('primary', { theme })};
+          background: ${lighten(0.08, 'grey', { theme })};
+        }
+      `}
+    >
+      {children}
+    </li>
+  )
+}
 
 export default Item
