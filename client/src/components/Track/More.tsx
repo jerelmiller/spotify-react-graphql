8eb47a693cc0187220fe52a2cfd8d77f89dca896
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 00:21:52 -0700

    Add dummy track menu items

--- client/src/components/Track/More.tsx
+++ client/src/components/Track/More.tsx
@@ -3,6 +3,14 @@ import MoreMenu from '../MoreMenu'
 
 interface Props {}
 
-const More: FC<Props> = () => <MoreMenu size="1.25rem" />
+const More: FC<Props> = () => (
+  <MoreMenu size="1.25rem" align="right">
+    <MoreMenu.Item>Start Radio</MoreMenu.Item>
+    <MoreMenu.Item>Remove From Your Library</MoreMenu.Item>
+    <MoreMenu.Item>Add to Queue</MoreMenu.Item>
+    <MoreMenu.Item>Add to Playlist</MoreMenu.Item>
+    <MoreMenu.Item>Copy Song Link</MoreMenu.Item>
+  </MoreMenu>
+)
 
 export default More
