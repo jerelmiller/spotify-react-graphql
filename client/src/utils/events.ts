60e5be851ce882402bdb3b1358fa0d0b7f4bd024
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 00:44:16 -0700

    Add helper to compose event handlers

---
+++ client/src/utils/events.ts
@@ -0,0 +1,9 @@
+import { MouseEvent, SyntheticEvent } from 'react'
+
+type EventHandler<Event extends SyntheticEvent> =
+  | ((e: Event) => void)
+  | undefined
+
+export const composeHandlers = <Event extends SyntheticEvent>(
+  ...fns: EventHandler<Event>[]
+) => (e: Event) => fns.forEach(fn => fn && fn(e))
