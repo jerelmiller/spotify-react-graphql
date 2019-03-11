1e780380f493b220c4d59c72555c9a69a63338b2
Jerel Miller <jerelmiller@gmail.com>
2019-03-10 20:24:04 -0700

    Export ColorName from theme

--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -2,6 +2,7 @@ import colors from './colors'
 import typography from './typography'
 
 export type Theme = typeof theme
+export type ColorName = keyof typeof colors
 
 const theme = { colors, typography }
 
