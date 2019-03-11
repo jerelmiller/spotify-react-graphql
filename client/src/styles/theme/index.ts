7eb759be8266cc96415643f50ed4b8e4f117c65a
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 00:01:46 -0700

    Add fontSize helper

--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -3,6 +3,7 @@ import typography from './typography'
 
 export type Theme = typeof theme
 export type ColorName = keyof typeof colors
+export type FontSize = keyof typeof typography.size
 
 const theme = { colors, typography }
 
--- client/src/styles/utils.ts
+++ client/src/styles/utils.ts
@@ -1,7 +1,7 @@
 import { curry } from '../utils/fp'
 import { css } from '@emotion/core'
 import { lighten as lighter } from 'polished'
-import { ColorName, Theme } from './theme'
+import { ColorName, FontSize, Theme } from './theme'
 
 interface Props {
   theme: Theme
@@ -16,6 +16,12 @@ export const color = curry(
 
 export const textColor = curry(
   (name: TypographyColor, { theme }: Props) => theme.typography.colors[name]
+)
+
+export const fontSize = curry(
+  (size: FontSize, { theme }: Props) => css`
+    font-size: ${theme.typography.size[size]};
+  `
 )
 
 export const typography = curry((name: TypographyStyle, { theme }: Props) => {
