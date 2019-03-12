a97ee20251f7bf7ea7b8658583872f7638c4b3b0
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 19:32:12 -0700

    Copy song link to clipboard

--- client/src/components/Track/More.tsx
+++ client/src/components/Track/More.tsx
@@ -1,16 +1,37 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import MoreMenu from '../MoreMenu'
+import copyToClipboard from '../../utils/copyToClipboard'
+import Notify from '../NotifyMutation'
+import TrackContext from './Context'
+import useLocation from '../../hooks/useLocation'
 
 interface Props {}
 
-const More: FC<Props> = () => (
-  <MoreMenu size="1.25rem" align="right">
-    <MoreMenu.Item>Start Radio</MoreMenu.Item>
-    <MoreMenu.Item>Remove From Your Library</MoreMenu.Item>
-    <MoreMenu.Item>Add to Queue</MoreMenu.Item>
-    <MoreMenu.Item>Add to Playlist</MoreMenu.Item>
-    <MoreMenu.Item>Copy Song Link</MoreMenu.Item>
-  </MoreMenu>
-)
+const More: FC<Props> = () => {
+  const { track } = useContext(TrackContext)
+  const { origin } = useLocation()
+
+  return (
+    <MoreMenu size="1.25rem" align="right">
+      <MoreMenu.Item>Start Radio</MoreMenu.Item>
+      <MoreMenu.Item>Remove From Your Library</MoreMenu.Item>
+      <MoreMenu.Item>Add to Queue</MoreMenu.Item>
+      <MoreMenu.Item>Add to Playlist</MoreMenu.Item>
+      <Notify>
+        {({ notify }) => (
+          <MoreMenu.Item
+            onClick={() =>
+              copyToClipboard(`${origin}/tracks/${track.id}`).then(() =>
+                notify({ message: 'Link copied to clipboard' })
+              )
+            }
+          >
+            Copy Song Link
+          </MoreMenu.Item>
+        )}
+      </Notify>
+    </MoreMenu>
+  )
+}
 
 export default More
