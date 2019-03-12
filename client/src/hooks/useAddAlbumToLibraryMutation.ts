b0829f7b15d162efbb617580884aa3b6d62a7947
Jerel Miller <jerelmiller@gmail.com>
2019-03-12 00:01:09 -0700

    Add useAddAlbumToLibraryMutation hook

---
+++ client/src/hooks/useAddAlbumToLibraryMutation.ts
@@ -0,0 +1,23 @@
+import { useCallback } from 'react'
+import { useMutation } from 'react-apollo-hooks'
+import gql from 'graphql-tag'
+
+const useAddAlbumToLibraryMutation = () => {
+  const addAlbumToLibary = useMutation(gql`
+    mutation AddAlbumToLibraryMutation($input: AddAlbumToLibraryInput!) {
+      addAlbumToLibrary(input: $input) {
+        album {
+          id
+          savedToLibrary
+        }
+      }
+    }
+  `)
+
+  return useCallback(
+    albumId => addAlbumToLibary({ variables: { input: { albumId } } }),
+    [addAlbumToLibary]
+  )
+}
+
+export default useAddAlbumToLibraryMutation
