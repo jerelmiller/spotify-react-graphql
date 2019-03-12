c2830ff8d7aa7cb26db8142ff64a87cd53953c1b
Jerel Miller <jerelmiller@gmail.com>
2019-03-12 00:47:06 -0700

    Add hook for toggling album in library

---
+++ client/src/hooks/useAlbumInLibraryToggle.ts
@@ -0,0 +1,48 @@
+import { useState, useCallback } from 'react'
+import useNotifyMutation from './useNotifyMutation'
+import useRemoveAlbumFromLibraryMutation from './useRemoveAlbumFromLibraryMutation'
+import useAddAlbumToLibraryMutation from './useAddAlbumToLibraryMutation'
+import PlusIcon from '../components/PlusIcon'
+import MinusIcon from '../components/MinusIcon'
+
+interface Album {
+  id: string
+  savedToLibrary: boolean
+}
+
+const useAlbumInLibraryToggle = ({ id, savedToLibrary }: Album) => {
+  const [loading, setLoading] = useState(false)
+  const notify = useNotifyMutation()
+  const removeAlbumFromLibrary = useRemoveAlbumFromLibraryMutation()
+  const addAlbumToLibrary = useAddAlbumToLibraryMutation()
+  const transaction = async (fn: () => Promise<void>) => {
+    setLoading(true)
+    await fn()
+    setLoading(false)
+  }
+
+  const add = useCallback(() => {
+    transaction(async () => {
+      await addAlbumToLibrary(id)
+      notify({ message: 'Saved to Your Library' })
+    })
+  }, [addAlbumToLibrary, id])
+
+  const remove = useCallback(() => {
+    transaction(async () => {
+      await removeAlbumFromLibrary(id)
+      notify({ message: 'Removed from Your Library' })
+    })
+  }, [removeAlbumFromLibrary, id])
+
+  return {
+    loading,
+    message: savedToLibrary
+      ? 'Remove from Your Library'
+      : 'Save to Your Library',
+    toggle: savedToLibrary ? remove : add,
+    Icon: savedToLibrary ? MinusIcon : PlusIcon
+  }
+}
+
+export default useAlbumInLibraryToggle
