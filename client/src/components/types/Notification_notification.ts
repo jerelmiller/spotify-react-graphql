ba5f84442fac8920bd65b876bc2fdf01a18a18c9
Jerel Miller <jerelmiller@gmail.com>
2019-03-11 19:45:32 -0700

    WIP notification components

---
+++ client/src/components/types/Notification_notification.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: Notification_notification
+// ====================================================
+
+export interface Notification_notification {
+  __typename: "Notification";
+  id: string;
+  message: string;
+  timeout: number;
+}
