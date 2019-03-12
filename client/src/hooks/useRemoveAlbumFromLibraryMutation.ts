fae65a0c83b2e13ccecdf485d1895f385fa2e339
Jerel Miller <jerelmiller@gmail.com>
2019-03-12 00:03:24 -0700

    Add useRemoveAlbumFromLibraryMutation hook

---
+++ client/src/hooks/useRemoveAlbumFromLibraryMutation.ts
@@ -0,0 +1,25 @@
+import { useCallback } from 'react'
+import { useMutation } from 'react-apollo-hooks'
+import gql from 'graphql-tag'
+
+const useRemoveAlbumFromLibraryMutation = () => {
+  const removeAlbumFromLibrary = useMutation(gql`
+    mutation RemoveAlbumFromLibraryMutation(
+      $input: RemoveAlbumFromLibraryInput!
+    ) {
+      removeAlbumFromLibrary(input: $input) {
+        album {
+          id
+          savedToLibrary
+        }
+      }
+    }
+  `)
+
+  return useCallback(
+    albumId => removeAlbumFromLibrary({ variables: { input: { albumId } } }),
+    [removeAlbumFromLibrary]
+  )
+}
+
+export default useRemoveAlbumFromLibraryMutation
