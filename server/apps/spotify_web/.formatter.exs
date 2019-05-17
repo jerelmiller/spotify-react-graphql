c4f9376fca7038740e0e9d0755e17928fc72d907
Jerel Miller <jmiller@newrelic.com>
2019-05-17 16:51:14 -0700

    add absinthe library to formatter

--- server/apps/spotify_web/.formatter.exs
+++ server/apps/spotify_web/.formatter.exs
@@ -1,8 +1,4 @@
 [
-  import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    field: 2,
-    field: 3
-  ]
+  import_deps: [:phoenix, :absinthe],
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
 ]
