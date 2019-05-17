2b9e27954ba17c21edf3fdbfe5c420cd9d92cac1
Jerel Miller <jmiller@newrelic.com>
2019-05-16 23:54:14 -0700

    remove unused plugs

--- server/apps/spotify_web/lib/spotify_web/endpoint.ex
+++ server/apps/spotify_web/lib/spotify_web/endpoint.ex
@@ -5,16 +5,6 @@ defmodule SpotifyWeb.Endpoint do
     websocket: true,
     longpoll: false
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :spotify_web,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
@@ -31,14 +21,6 @@ defmodule SpotifyWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_spotify_web_key",
-    signing_salt: "iAM8uaKm"
 
   plug SpotifyWeb.Router
 end
