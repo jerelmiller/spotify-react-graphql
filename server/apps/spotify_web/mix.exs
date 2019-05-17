7f4db45248e3280301140d84d29d26a3b3e2ef5d
Jerel Miller <jmiller@newrelic.com>
2019-05-17 02:37:45 -0700

    Configure cors

--- server/apps/spotify_web/mix.exs
+++ server/apps/spotify_web/mix.exs
@@ -47,6 +47,7 @@ defmodule SpotifyWeb.MixProject do
       {:spotify_client, in_umbrella: true},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:cors_plug, "~> 2.0"},
       {:new_relic_agent, "~> 1.0"},
       {:new_relic_phoenix, "~> 0.1"},
       {:new_relic_absinthe, "~> 0.0.2"}
