4fac205e5b512235863b1e849548cb0b20ca3ece
Jerel Miller <jmiller@newrelic.com>
2019-05-17 00:42:37 -0700

    Generate spotify client app

---
+++ server/apps/spotify_client/lib/spotify_client.ex
@@ -0,0 +1,18 @@
+defmodule SpotifyClient do
+  @moduledoc """
+  Documentation for SpotifyClient.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpotifyClient.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
