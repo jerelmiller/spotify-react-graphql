8ff898aca8a1ceb9bbd07dabd8b36c52817b30a6
Jerel Miller <jmiller@newrelic.com>
2019-05-17 02:38:03 -0700

    Add field/2 to formatter

--- server/apps/spotify_web/.formatter.exs
+++ server/apps/spotify_web/.formatter.exs
@@ -1,4 +1,7 @@
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    field: 2
+  ]
 ]
