2f727c8554615137fdc95cdbe0e5b0f38bc3efaa
Jerel Miller <jmiller@newrelic.com>
2019-05-17 02:14:44 -0700

    parse json

--- server/apps/spotify_client/lib/spotify_client/client.ex
+++ server/apps/spotify_client/lib/spotify_client/client.ex
@@ -7,6 +7,23 @@ defmodule SpotifyClient.Client do
   def uri(:auth, path), do: @oauth_uri <> path
   def uri(:api, path), do: @api_uri <> path
 
-  def post(uri, body, headers \\ [], options \\ []),
-    do: HTTPoison.post(uri, body, headers, options)
+  def post(uri, body, headers \\ [], options \\ []) do
+    uri
+    |> HTTPoison.post(body, headers, options)
+    |> parse_response(:json)
+  end
+
+  defp parse_response({:ok, %{status_code: 200} = response}, :json) do
+    {:ok,
+     response
+     |> Map.update!(:body, &Jason.decode!(&1))}
+  end
+
+  defp parse_response({:ok, response}, :json) do
+    {:error,
+     response
+     |> Map.update!(:body, &Jason.decode!(&1))}
+  end
+
+  defp parse_response(response, _), do: response
 end
