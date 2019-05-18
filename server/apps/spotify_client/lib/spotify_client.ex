6dcf5d30e6371577b398f2912ccc5a3972118454
Jerel Miller <jmiller@newrelic.com>
2019-05-17 22:02:14 -0700

    Move uri function to top-level client

--- server/apps/spotify_client/lib/spotify_client.ex
+++ server/apps/spotify_client/lib/spotify_client.ex
@@ -3,6 +3,7 @@ defmodule SpotifyClient do
   alias SpotifyClient.Client
 
   @oauth_uri "https://accounts.spotify.com"
+  @api_uri "https://api.spotify.com/v1"
 
   def client_id, do: Config.client_id()
   def client_secret, do: Config.client_secret()
@@ -10,7 +11,7 @@ defmodule SpotifyClient do
   def oauth_uri, do: @oauth_uri
 
   def generate_token(code, redirect_uri) do
-    Client.uri(:auth, "/api/token")
+    oauth_uri("/api/token")
     |> Client.post(
       {:form,
        [
@@ -24,8 +25,36 @@ defmodule SpotifyClient do
     )
   end
 
-  def playlists(params \\ [], headers \\ []) do
-    Client.uri(:api, "/me/playlists")
+  def album(id, headers \\ []) do
+    "/albums/#{id}"
+    |> api_uri()
     |> Client.get(headers)
   end
+
+  def artist(id, headers \\ []) do
+    "/artists/#{id}"
+    |> api_uri()
+    |> Client.get(headers)
+  end
+
+  def playlists(params \\ %{}, headers \\ []) do
+    "/me/playlists"
+    |> api_uri(params)
+    |> Client.get(headers)
+  end
+
+  def categories(params \\ %{}, headers \\ []) do
+    "/browse/categories"
+    |> api_uri(params)
+    |> Client.get(headers)
+  end
+
+  defp uri(path, nil), do: path
+  defp uri(path, %{}), do: path
+  defp uri(path, params), do: "#{path}?#{query_params(params)}"
+
+  defp api_uri(path, params \\ nil), do: @api_uri <> uri(path, params)
+  defp oauth_uri(path, params \\ nil), do: @oauth_uri <> uri(path, params)
+
+  defp query_params(params) when is_map(params), do: URI.encode_query(params)
 end
