cc28ad9c19b00a1cf43adedcbc62079f5f9118f9
Jerel Miller <jmiller@newrelic.com>
2019-05-17 22:11:13 -0700

    Merge files for spotify client

--- server/apps/spotify_client/lib/spotify_client.ex
+++ server/apps/spotify_client/lib/spotify_client.ex
@@ -1,6 +1,6 @@
 defmodule SpotifyClient do
   alias SpotifyClient.Config
-  alias SpotifyClient.Client
+  alias NewRelic.Instrumented.HTTPoison
 
   @oauth_uri "https://accounts.spotify.com"
   @api_uri "https://api.spotify.com/v1"
@@ -12,7 +12,7 @@ defmodule SpotifyClient do
 
   def generate_token(code, redirect_uri) do
     oauth_uri("/api/token")
-    |> Client.post(
+    |> post(
       {:form,
        [
          grant_type: "authorization_code",
@@ -28,25 +28,25 @@ defmodule SpotifyClient do
   def album(id, headers \\ []) do
     "/albums/#{id}"
     |> api_uri()
-    |> Client.get(headers)
+    |> get(headers)
   end
 
   def artist(id, headers \\ []) do
     "/artists/#{id}"
     |> api_uri()
-    |> Client.get(headers)
+    |> get(headers)
   end
 
   def playlists(params \\ %{}, headers \\ []) do
     "/me/playlists"
     |> api_uri(params)
-    |> Client.get(headers)
+    |> get(headers)
   end
 
   def categories(params \\ %{}, headers \\ []) do
     "/browse/categories"
     |> api_uri(params)
-    |> Client.get(headers)
+    |> get(headers)
   end
 
   defp uri(path, nil), do: path
@@ -57,4 +57,30 @@ defmodule SpotifyClient do
   defp oauth_uri(path, params \\ nil), do: @oauth_uri <> uri(path, params)
 
   defp query_params(params) when is_map(params), do: URI.encode_query(params)
+
+  defp get(uri, headers \\ [], options \\ []) do
+    uri
+    |> HTTPoison.get(headers, options)
+    |> parse_response()
+  end
+
+  def post(uri, body, headers \\ [], options \\ []) do
+    uri
+    |> HTTPoison.post(body, headers, options)
+    |> parse_response()
+  end
+
+  defp parse_response({:ok, %{status_code: 200} = response}) do
+    {:ok,
+     response
+     |> Map.update!(:body, &Jason.decode!(&1, keys: :atoms))}
+  end
+
+  defp parse_response({:ok, response}) do
+    {:error,
+     response
+     |> Map.update!(:body, &Jason.decode!(&1, keys: :atoms))}
+  end
+
+  defp parse_response(response), do: response
 end
