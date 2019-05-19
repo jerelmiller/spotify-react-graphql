b797bd3d070f13f4f2ef1a4ecf170047d789a0e0
Jerel Miller <jmiller@newrelic.com>
2019-05-18 21:51:36 -0700

    Fix wrong fields on playable_collection. Add resolve_type/1

--- server/apps/spotify_web/lib/spotify_web/schema/interfaces.ex
+++ server/apps/spotify_web/lib/spotify_web/schema/interfaces.ex
@@ -2,18 +2,20 @@ defmodule SpotifyWeb.Schema.Interfaces do
   use Absinthe.Schema.Notation
 
   interface :playable_collection do
-    field :id, non_null(:id)
-    field :collaborative, non_null(:boolean)
+    @desc "Images associated with the playable collection"
     field :images, list_of(:image)
-    field :name, non_null(:string)
-    field :owner, non_null(:user)
-    field :public, non_null(:boolean)
-    field :tracks, non_null(:playlist_track_connection)
 
     @desc """
     The [Spotify URI](https://developer.spotify.com/documentation/web-api/#spotify-uris-and-ids)
     for the playlist.
     """
     field :uri, :string
+
+    resolve_type fn %{uri: uri} ->
+      uri
+      |> String.split(':')
+      |> Enum.at(1)
+      |> String.to_existing_atom()
+    end
   end
 end
