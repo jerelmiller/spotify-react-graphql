f6df4e7ea84fc7fede1a8f8a81f3b52d9fb1f433
Jerel Miller <jmiller@newrelic.com>
2019-05-18 22:06:06 -0700

    Add simple artist and artists field to album

--- server/apps/spotify_web/lib/spotify_web/schema.ex
+++ server/apps/spotify_web/lib/spotify_web/schema.ex
@@ -1,7 +1,7 @@
 defmodule SpotifyWeb.Schema do
   use Absinthe.Schema
 
-  alias SpotifyWeb.Resolvers
+  alias SpotifyWeb.Resolvers.{Album, Category, Playlist, Viewer}
 
   import_types SpotifyWeb.Schema.Types
   import_types SpotifyWeb.Schema.Interfaces
@@ -10,21 +10,21 @@ defmodule SpotifyWeb.Schema do
     field :category, :category do
       arg :id, non_null(:id)
 
-      resolve &Resolvers.Category.find/2
+      resolve &Category.find/2
     end
 
     field :categories, :category_connection do
-      arg :limit, :integer
-      arg :offset, :integer
+      arg :limit, :integer, default_value: 50
+      arg :offset, :integer, default_value: 0
 
-      resolve &Resolvers.Category.categories/2
+      resolve &Category.categories/2
     end
 
     field :new_releases, :album_connection do
-      arg :limit, :integer
-      arg :offset, :integer
+      arg :limit, :integer, default_value: 50
+      arg :offset, :integer, default_value: 0
 
-      resolve &Resolvers.Album.new_releases/2
+      resolve &Album.new_releases/2
     end
 
     field :playlists_by_category, :playlist_connection do
@@ -32,11 +32,11 @@ defmodule SpotifyWeb.Schema do
       arg :limit, :integer, default_value: 50
       arg :offset, :integer, default_value: 0
 
-      resolve &Resolvers.Playlist.by_category/2
+      resolve &Playlist.by_category/2
     end
 
     @desc "Info about the current logged-in user"
-    field :viewer, :viewer, resolve: &Resolvers.Viewer.viewer/2
+    field :viewer, :viewer, resolve: &Viewer.viewer/2
   end
 
   def middleware(middleware, _field, _object) do
