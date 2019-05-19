f2949f0e21846a7d10b4d285c16cfc3db2d8116a
Jerel Miller <jmiller@newrelic.com>
2019-05-19 01:35:09 -0700

    Add helper to prepare headers

--- server/apps/spotify_web/lib/spotify_web/resolvers/helpers.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/helpers.ex
@@ -1,4 +1,7 @@
 defmodule SpotifyWeb.Resolvers.Helpers do
+  def prepare_headers(%{context: %{authorization: authorization}}),
+    do: [{"Authorization", authorization}]
+
   def handle_response(response, path \\ fn body -> body end)
 
   def handle_response({:ok, %HTTPoison.Response{body: body}}, result_path) do
