edc9dedd9914a5cbd93f553e49f1547cee480c27
Jerel Miller <jmiller@newrelic.com>
2019-05-19 01:16:14 -0700

    fetch info for current user

--- server/apps/spotify_web/lib/spotify_web/resolvers/viewer.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/viewer.ex
@@ -1,4 +1,6 @@
 defmodule SpotifyWeb.Resolvers.Viewer do
+  alias SpotifyWeb.Resolvers.Helpers
+
   def viewer(_, _), do: {:ok, %{}}
 
   def saved_albums(args, %{context: %{authorization: authorization}}) do
@@ -17,5 +19,10 @@ defmodule SpotifyWeb.Resolvers.Viewer do
       {:ok, %HTTPoison.Response{body: body}} -> {:ok, body}
       error -> error
     end
+  end
+
+  def user(_, %{context: %{authorization: authorization}}) do
+    SpotifyClient.current_user([{"Authorization", authorization}])
+    |> Helpers.handle_response()
   end
 end
