16fa0446b0acadb6f79e7d8d45cf6b1a34d46f78
Jerel Miller <jmiller@newrelic.com>
2019-05-18 22:05:56 -0700

    Fix wrong pattern match. Add primary artist resolver

--- server/apps/spotify_web/lib/spotify_web/resolvers/album.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/album.ex
@@ -1,10 +1,12 @@
 defmodule SpotifyWeb.Resolvers.Album do
-  def new_releases(args, %{context: %{authentication: authentication}}) do
+  def new_releases(args, %{context: %{authorization: authorization}}) do
     args
-    |> SpotifyClient.new_releases([{"Authentication", authentication}])
+    |> SpotifyClient.new_releases([{"Authorization", authorization}])
     |> case do
       {:ok, %HTTPoison.Response{body: body}} -> {:ok, body.albums}
       error -> error
     end
   end
+
+  def primary_artist(%{albums: albums}, _, __), do: List.first(albums)
 end
