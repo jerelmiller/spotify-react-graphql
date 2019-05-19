1bb0398e3d30d3ab53c4db1f62c53526ba97060b
Jerel Miller <jmiller@newrelic.com>
2019-05-18 22:44:20 -0700

    Fix node resolution for saved album edge

--- server/apps/spotify_web/lib/spotify_web/resolvers/connection.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/connection.ex
@@ -4,4 +4,15 @@ defmodule SpotifyWeb.Resolvers.Connection do
 
   def node(item, _, _) when is_nil(item), do: {:error, "Node is null"}
   def node(item, _, _), do: {:ok, item}
+
+  def named_node(name) do
+    fn parent, _, _ ->
+      parent
+      |> Map.fetch(name)
+      |> case do
+        :error -> {:error, "key does not exist"}
+        result -> result
+      end
+    end
+  end
 end
