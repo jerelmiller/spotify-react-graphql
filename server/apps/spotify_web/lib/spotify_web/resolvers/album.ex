fd2d925cc821411398855a0cb8ae4648f9b68067
Jerel Miller <jmiller@newrelic.com>
2019-05-18 23:38:23 -0700

    Get al album by its id

--- server/apps/spotify_web/lib/spotify_web/resolvers/album.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/album.ex
@@ -1,11 +1,16 @@
 defmodule SpotifyWeb.Resolvers.Album do
+  alias SpotifyWeb.Resolvers.Helpers
+
+  def find(%{id: id}, %{context: %{authorization: authorization}}) do
+    id
+    |> SpotifyClient.album([{"Authorization", authorization}])
+    |> Helpers.handle_response()
+  end
+
   def new_releases(args, %{context: %{authorization: authorization}}) do
     args
     |> SpotifyClient.new_releases([{"Authorization", authorization}])
-    |> case do
-      {:ok, %HTTPoison.Response{body: body}} -> {:ok, body.albums}
-      error -> error
-    end
+    |> Helpers.handle_response(fn %{albums: albums} -> albums end)
   end
 
   def primary_artist(%{albums: albums}, _, __), do: List.first(albums)
