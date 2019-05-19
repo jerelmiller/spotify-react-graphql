c2b77dbf4ac06cc9178e939286cf23422d6df3f9
Jerel Miller <jmiller@newrelic.com>
2019-05-18 21:17:46 -0700

    Add ability to get a category or playlists by a category

--- server/apps/spotify_web/lib/spotify_web/resolvers/category.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/category.ex
@@ -1,4 +1,13 @@
 defmodule SpotifyWeb.Resolvers.Category do
+  def find(%{id: id}, %{context: %{authorization: authorization}}) do
+    id
+    |> SpotifyClient.category([{"Authorization", authorization}])
+    |> case do
+      {:ok, %HTTPoison.Response{body: body}} -> {:ok, body}
+      error -> error
+    end
+  end
+
   def categories(args, %{context: %{authorization: authorization}}) do
     args
     |> SpotifyClient.categories([{"Authorization", authorization}])
--- server/apps/spotify_web/lib/spotify_web/schema.ex
+++ server/apps/spotify_web/lib/spotify_web/schema.ex
@@ -6,8 +6,11 @@ defmodule SpotifyWeb.Schema do
   import_types(SpotifyWeb.Schema.Types)
 
   query do
-    @desc "Info about the current logged-in user"
-    field :viewer, :viewer, resolve: &Resolvers.Viewer.viewer/2
+    field :category, :category do
+      arg :id, non_null(:id)
+
+      resolve &Resolvers.Category.find/2
+    end
 
     field :categories, :category_connection do
       arg :limit, :integer
@@ -15,6 +18,17 @@ defmodule SpotifyWeb.Schema do
 
       resolve &Resolvers.Category.categories/2
     end
+
+    field :playlists_by_category, :playlist_connection do
+      arg :category_id, non_null(:id)
+      arg :limit, :integer, default_value: 50
+      arg :offset, :integer, default_value: 0
+
+      resolve &Resolvers.Playlist.by_category/2
+    end
+
+    @desc "Info about the current logged-in user"
+    field :viewer, :viewer, resolve: &Resolvers.Viewer.viewer/2
   end
 
   def middleware(middleware, _field, _object) do
