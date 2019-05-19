24e251e31529960dae295cca91dd7de2ce95bc67
Jerel Miller <jmiller@newrelic.com>
2019-05-18 20:46:03 -0700

    Fix page info resolver

--- server/apps/spotify_web/lib/spotify_web/resolvers/page_info.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/page_info.ex
@@ -1,14 +1,11 @@
 defmodule SpotifyWeb.Resolvers.PageInfo do
-  def limit(%{"limit" => limit}, _, _), do: {:ok, limit}
+  def has_next_page(%{next: ""}, _, _), do: {:ok, false}
+  def has_next_page(%{next: nil}, _, _), do: {:ok, false}
+  def has_next_page(%{next: _}, _, _), do: {:ok, true}
+  def has_next_page{_, _, _}, do: {:error, "Unable to determine if has next page"}
 
-  def has_next_page(%{"next" => ""}, _, _), do: false
-  def has_next_page(%{"next" => nil}, _, _), do: false
-  def has_next_page(%{"next" => _}, _, _), do: true
-
-  def has_previous_page(%{"previous" => ""}, _, _), do: false
-  def has_previous_page(%{"previous" => nil}, _, _), do: false
-  def has_previous_page(%{"previous" => _}, _, _), do: true
-
-  def total(%{"total" => total}, _, _), do: {:ok, total}
-  def offset(%{"offset" => offset}, _, _), do: {:ok, offset}
+  def has_previous_page(%{previous: ""}, _, _), do: {:ok, false}
+  def has_previous_page(%{previous: nil}, _, _), do: {:ok, false}
+  def has_previous_page(%{previous: _}, _, _), do: {:ok, true}
+  def has_previous_page{_, _, _}, do: {:error, "Unable to determine if has previous page"}
 end
