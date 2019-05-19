25d498e56cf99eec996ba9df001e6fc198110e1a
Jerel Miller <jmiller@newrelic.com>
2019-05-18 20:33:15 -0700

    Hook up categories call to spotify api

--- server/apps/spotify_web/lib/spotify_web/resolvers/category.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/category.ex
@@ -1,5 +1,10 @@
 defmodule SpotifyWeb.Resolvers.Category do
-  def categories(_, _) do
-    {:ok, %{}}
+  def categories(args, %{context: %{authorization: authorization}}) do
+    args
+    |> SpotifyClient.categories([{"Authorization", authorization}])
+    |> case do
+      {:ok, %HTTPoison.Response{body: body}} -> {:ok, body.categories}
+      error -> error
+    end
   end
 end
