e1d6d4d635577f565c3db1a8dacefc071b383fe3
Jerel Miller <jmiller@newrelic.com>
2019-05-19 01:35:24 -0700

    get related artists for an artist

--- server/apps/spotify_web/lib/spotify_web/resolvers/artist.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/artist.ex
@@ -13,6 +13,12 @@ defmodule SpotifyWeb.Resolvers.Artist do
     |> Helpers.handle_response()
   end
 
+  def related_artists(%{id: id}, _, res) do
+    id
+    |> SpotifyClient.related_artists_for_artist(Helpers.prepare_headers(res))
+    |> Helpers.handle_response(fn %{artists: artists} -> artists end)
+  end
+
   def top_tracks(%{id: id}, %{limit: limit}, %{context: %{authorization: authorization}}) do
     id
     |> SpotifyClient.top_tracks_by_artist([{"Authorization", authorization}])
