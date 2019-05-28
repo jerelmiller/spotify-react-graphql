2efc7b9553b5163ee6715e53de52b481e2cbc7d6
Jerel Miller <jmiller@newrelic.com>
2019-05-27 23:50:39 -0700

    Add tracks field for a playlist

--- server/apps/spotify_web/lib/spotify_web/resolvers/playlist.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/playlist.ex
@@ -1,4 +1,6 @@
 defmodule SpotifyWeb.Resolvers.Playlist do
+  alias SpotifyWeb.Resolvers.Helpers
+
   def by_category(%{category_id: category_id} = args, %{context: %{authorization: authorization}}) do
     category_id
     |> SpotifyClient.playlists_by_category(Map.delete(args, :category_id), [
@@ -8,5 +10,11 @@ defmodule SpotifyWeb.Resolvers.Playlist do
       {:ok, %HTTPoison.Response{body: body}} -> {:ok, body.playlists}
       error -> error
     end
+  end
+
+  def tracks(%{id: id}, args, res) do
+    id
+    |> SpotifyClient.tracks_by_playlist(args, Helpers.prepare_headers(res))
+    |> Helpers.handle_response()
   end
 end
