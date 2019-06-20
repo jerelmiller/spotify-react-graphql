50b560e9ff08f232bd34c442ae92448e282f9f8c
Jerel Miller <jerelmiller@gmail.com>
2019-06-19 23:26:39 -0700

    Add a resolver error protocol to better format errors

---
+++ server/apps/spotify_web/lib/spotify_web/resolver_error.ex
@@ -0,0 +1,31 @@
+defprotocol SpotifyWeb.ResolverError do
+  def message(error)
+  def code(error)
+end
+
+defimpl SpotifyWeb.ResolverError, for: BitString do
+  def message(error), do: {:ok, error}
+  def code(_), do: :ok
+end
+
+defimpl SpotifyWeb.ResolverError, for: Map do
+  def message(%{message: message}), do: {:ok, message}
+  def message(_), do: :error
+
+  def code(%{code: code}), do: {:ok, code}
+  def code(_), do: :ok
+end
+
+defimpl SpotifyWeb.ResolverError, for: HTTPoison.Response do
+  def message(%{body: %{error: %{message: message}}}), do: {:ok, message}
+  def message(_), do: :error
+
+  def code(%{status_code: status_code}), do: {:ok, status_string(status_code)}
+
+  def code(response) do
+    IO.inspect(response)
+    :ok
+  end
+
+  defp status_string(401), do: "UNAUTHENTICATED"
+end
