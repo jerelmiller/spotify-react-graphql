b722586d1ccd99a1bdbee04ffa196443ae4b9caa
Jerel Miller <jerelmiller@gmail.com>
2019-06-20 16:04:46 -0700

    Better error when unable to find session

--- server/apps/spotify_web/lib/spotify_web/resolvers/auth.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/auth.ex
@@ -2,12 +2,23 @@ defmodule SpotifyWeb.Resolvers.Auth do
   alias Spotify.Sessions
 
   def refresh_session(%{input: input}, _) do
-    with %Sessions.Session{} = session <- Sessions.find_by_access_token(input.token),
+    with %Sessions.Session{} = session <-
+           Sessions.find_by_access_token(input.token),
          {:ok, %{body: %{access_token: access_token} = body}} <-
-           SpotifyClient.refresh_session(session.access_token, session.refresh_token),
+           SpotifyClient.refresh_session(
+             session.access_token,
+             session.refresh_token
+           ),
          {:ok, session} <-
-           Sessions.update(session, Map.take(body, [:access_token, :scope, :expires_in])) do
+           Sessions.update(
+             session,
+             Map.take(body, [:access_token, :scope, :expires_in])
+           ) do
       {:ok, %{token: access_token}}
+    else
+      nil ->
+        {:error,
+         %{message: "Unable to find current session", code: "UNAUTHENTICATED"}}
     end
   end
 end
