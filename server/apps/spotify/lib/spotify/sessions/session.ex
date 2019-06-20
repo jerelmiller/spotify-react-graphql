5d3d7423841214d8793ebecaee065a2da8769391
Jerel Miller <jerelmiller@gmail.com>
2019-06-20 14:32:59 -0700

    Move expires_in logic to schema

--- server/apps/spotify/lib/spotify/sessions/session.ex
+++ server/apps/spotify/lib/spotify/sessions/session.ex
@@ -6,19 +6,32 @@ defmodule Spotify.Sessions.Session do
     field :refresh_token, :string
     field :expires_at, :utc_datetime
     field :scope, :string
+    field :expires_in, :integer, virtual: true
 
     timestamps()
   end
 
   def create_changeset(%__MODULE__{} = session, attrs) do
     session
-    |> cast(attrs, [:access_token, :refresh_token, :expires_at, :scope])
-    |> validate_required([:access_token, :refresh_token, :expires_at, :scope])
+    |> cast(attrs, [:access_token, :refresh_token, :expires_in, :scope])
+    |> validate_required([:access_token, :refresh_token, :expires_in, :scope])
+    |> put_expires_at()
   end
 
   def update_changeset(%__MODULE__{} = session, attrs) do
     session
-    |> cast(attrs, [:access_token, :expires_at, :scope])
-    |> validate_required([:access_token, :expires_at, :scope])
+    |> cast(attrs, [:access_token, :expires_in, :scope])
+    |> validate_required([:access_token, :expires_in, :scope])
+    |> put_expires_at()
+  end
+
+  defp put_expires_at(changeset) do
+    put_change(
+      changeset,
+      :expires_at,
+      DateTime.utc_now()
+      |> DateTime.add(get_change(changeset, :expires_in), :second)
+      |> DateTime.truncate(:second)
+    )
   end
 end
