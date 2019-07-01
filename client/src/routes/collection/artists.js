5502992136aae494c3ad19584b24515243fd62e9
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 16:57:27 -0700

    Convert artists to use hooks

--- client/src/routes/collection/artists.js
+++ client/src/routes/collection/artists.js
@@ -5,46 +5,47 @@ import gql from 'graphql-tag'
 import PageTitle from 'components/PageTitle'
 import TileGrid from 'components/TileGrid'
 import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
 
-const Artists = () => (
-  <Query
-    query={gql`
-      query ArtistsQuery($limit: Int!, $after: String) {
-        viewer {
-          followedArtists(limit: $limit, after: $after) {
-            edges {
-              node {
-                id
-                ...Artist_artist
-              }
+const Artists = () => {
+  const {
+    loading,
+    data: { viewer }
+  } = useQuery(gql`
+    query ArtistsQuery($limit: Int, $after: String) {
+      viewer {
+        followedArtists(limit: $limit, after: $after) {
+          edges {
+            node {
+              id
+              ...Artist_artist
             }
           }
         }
       }
+    }
 
-      ${ArtistTile.fragments.artist}
-    `}
-    variables={{ limit: 50 }}
-  >
-    {({ loading, data: { viewer } }) => (
-      <>
-        <PageTitle>Artists</PageTitle>
-        <TileGrid minWidth="160px">
-          {loading ||
-            viewer.followedArtists.edges.map(({ node }, idx) => (
-              <Fragment key={node.id}>
-                {idx === 0 && (
-                  <BackgroundFromImage
-                    src={(node.images[1] || node.images[0]).url}
-                  />
-                )}
-                <ArtistTile artist={node} />
-              </Fragment>
-            ))}
-        </TileGrid>
-      </>
-    )}
-  </Query>
-)
+    ${ArtistTile.fragments.artist}
+  `)
+
+  return (
+    <>
+      <PageTitle>Artists</PageTitle>
+      <TileGrid minWidth="160px">
+        {loading ||
+          viewer.followedArtists.edges.map(({ node }, idx) => (
+            <Fragment key={node.id}>
+              {idx === 0 && (
+                <BackgroundFromImage
+                  src={(node.images[1] || node.images[0]).url}
+                />
+              )}
+              <ArtistTile artist={node} />
+            </Fragment>
+          ))}
+      </TileGrid>
+    </>
+  )
+}
 
 export default Artists
