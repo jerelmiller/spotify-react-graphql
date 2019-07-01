45428c384715c37c6e132c1379d48fb214f887d3
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 15:11:18 -0700

    Add pagination to albums list

--- client/src/routes/collection/albums.js
+++ client/src/routes/collection/albums.js
@@ -2,12 +2,19 @@ import React from 'react'
 import AlbumTile from 'components/AlbumTile'
 import gql from 'graphql-tag'
 import PageTitle from 'components/PageTitle'
+import PaginationObserver from 'components/PaginationObserver'
 import TileGrid from 'components/TileGrid'
 import useBackgroundColor from 'hooks/useBackgroundColor'
+import useScrollContainer from 'hooks/useScrollContainer'
 import { Query } from 'react-apollo'
+import { compose, concat, set, view, lensPath } from 'utils/fp'
+
+const edgesLens = lensPath(['viewer', 'savedAlbums', 'edges'])
+const pageInfoLens = lensPath(['viewer', 'savedAlbums', 'pageInfo'])
 
 const Albums = () => {
   useBackgroundColor('#090B0F')
+  const scrollContainer = useScrollContainer()
 
   return (
     <Query
@@ -21,15 +28,20 @@ const Albums = () => {
                   ...Album_album
                 }
               }
+
+              pageInfo {
+                ...PaginationObserver_pageInfo
+              }
             }
           }
         }
 
         ${AlbumTile.fragments.album}
+        ${PaginationObserver.fragments.pageInfo}
       `}
       variables={{ limit: 50, offset: 0 }}
     >
-      {({ loading, data: { viewer } }) => (
+      {({ loading, fetchMore, data: { viewer } }) => (
         <>
           <PageTitle>Albums</PageTitle>
           <TileGrid minWidth="180px">
@@ -38,6 +50,26 @@ const Albums = () => {
                 <AlbumTile key={node.id} album={node} />
               ))}
           </TileGrid>
+          {loading || (
+            <PaginationObserver
+              fetchMore={fetchMore}
+              scrollContainer={scrollContainer}
+              pageInfo={viewer.savedAlbums.pageInfo}
+              updateQuery={(prev, { fetchMoreResult }) =>
+                compose(
+                  set(
+                    edgesLens,
+                    concat(
+                      view(edgesLens, prev),
+                      view(edgesLens, fetchMoreResult)
+                    )
+                  ),
+                  set(pageInfoLens, view(pageInfoLens, fetchMoreResult))
+                )(prev)
+              }
+              threshold="750px"
+            />
+          )}
         </>
       )}
     </Query>
