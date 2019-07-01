01579cfb91ccd096e12882e87a777dada713ba85
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 01:21:30 -0700

    Use lenses to set values

--- client/src/utils/fp.ts
+++ client/src/utils/fp.ts
@@ -2,4 +2,15 @@ export const noop = () => {}
 export const value = <T = any>(val: T) => () => val
 export const prop = <K extends keyof T, T>(name: K) => (obj: T) => obj[name]
 
-export { curry, ifElse, compose, defaultTo } from 'ramda'
+export {
+  concat,
+  curry,
+  ifElse,
+  compose,
+  defaultTo,
+  lens,
+  lensPath,
+  lensProp,
+  view,
+  set
+} from 'ramda'
