3eb70e2549f7ae0d87d4430ccc8a2e9d0f89d287
Jerel Miller <jerelmiller@gmail.com>
2019-06-30 22:34:35 -0700

    Convert Track.Image back to js

--- client/src/components/Track/Image.js
+++ client/src/components/Track/Image.js
@@ -2,14 +2,9 @@ import React from 'react'
 import gql from 'graphql-tag'
 import LazyImage from '../LazyImage'
 import { useTrackContext } from './Context'
-import { FragmentComponent, GQLFragment } from '../../types/shared'
 import PlaceholderPhoto from '../PlaceholderPhoto'
 
-interface Props {
-  size: string
-}
-
-const Image: FragmentComponent<Props, { track: GQLFragment }> = ({ size }) => {
+const Image = ({ size }) => {
   const { track } = useTrackContext()
   const album = track.album.images[1]
 
