df474f718a5420ca4d17ce1b5ac4bfb2d2f88731
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 16:08:43 -0700

    Convert featured to use hooks

--- client/src/routes/browse/featured.js
+++ client/src/routes/browse/featured.js
@@ -4,42 +4,41 @@ import PageTitle from 'components/PageTitle'
 import PlaylistTile from 'components/PlaylistTile'
 import TileGrid from 'components/TileGrid'
 import useBackgroundColor from 'hooks/useBackgroundColor'
-import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
 
 const RouteBrowseFeatured = () => {
   useBackgroundColor('#1A101C')
 
-  return (
-    <Query
-      query={gql`
-        query BrowseFeaturedQuery($limit: Int, $offset: Int) {
-          featuredPlaylists(limit: $limit, offset: $offset) {
-            title
-            edges {
-              node {
-                id
-                ...PlaylistTile_playlist
-              }
-            }
+  const {
+    loading,
+    data: { featuredPlaylists }
+  } = useQuery(gql`
+    query BrowseFeaturedQuery($limit: Int, $offset: Int) {
+      featuredPlaylists(limit: $limit, offset: $offset) {
+        title
+        edges {
+          node {
+            id
+            ...PlaylistTile_playlist
           }
         }
-
-        ${PlaylistTile.fragments.playlist}
-      `}
-    >
-      {({ loading, data: { featuredPlaylists } }) =>
-        loading || (
-          <>
-            <PageTitle>{featuredPlaylists.title}</PageTitle>
-            <TileGrid minWidth="180px">
-              {featuredPlaylists.edges.map(({ node }) => (
-                <PlaylistTile key={node.id} playlist={node} />
-              ))}
-            </TileGrid>
-          </>
-        )
       }
-    </Query>
+    }
+
+    ${PlaylistTile.fragments.playlist}
+  `)
+
+  return (
+    loading || (
+      <>
+        <PageTitle>{featuredPlaylists.title}</PageTitle>
+        <TileGrid minWidth="180px">
+          {featuredPlaylists.edges.map(({ node }) => (
+            <PlaylistTile key={node.id} playlist={node} />
+          ))}
+        </TileGrid>
+      </>
+    )
   )
 }
 
