6536392038ba35be6e588b60a029bc3a5bcf32a2
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 23:51:27 -0700

    Inline the absinthe socket link

--- client/src/config/apollo.js
+++ client/src/config/apollo.js
@@ -1,3 +1,6 @@
+import * as AbsintheSocket from '@absinthe/socket'
+import { createAbsintheSocketLink } from '@absinthe/socket-apollo-link'
+import { Socket } from 'phoenix'
 import { ApolloLink, Observable, split } from 'apollo-link'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
@@ -15,7 +18,6 @@ import { setContext } from 'apollo-link-context'
 import introspectionQueryResultData from './fragmentTypes.json'
 import store from './store'
 import { compose, prop } from 'utils/fp'
-import absintheSocketLink from './absintheSocketLink'
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
 import { getMainDefinition } from 'apollo-utilities'
@@ -39,6 +41,18 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 })
 
 const cache = new InMemoryCache({ fragmentMatcher })
+
+const absintheSocketLink = createAbsintheSocketLink(
+  AbsintheSocket.create(
+    new Socket(`${process.env.REACT_APP_SOCKET_HOST}/socket`, {
+      params: {
+        get token() {
+          return getToken()
+        }
+      }
+    })
+  )
+)
 
 const retryAuthLink = onError(
   ({ graphQLErrors, networkErrors, operation, forward, response }) => {
