a8316d22da6ef34682091760dfd2e037626b809f
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 17:24:55 -0700

    Update useNotifyMutation to use official apollo hooks

--- client/src/hooks/useNotifyMutation.ts
+++ client/src/hooks/useNotifyMutation.ts
@@ -1,9 +1,9 @@
 import { useCallback } from 'react'
 import gql from 'graphql-tag'
-import { useMutation } from 'react-apollo-hooks'
+import { useMutation } from '@apollo/react-hooks'
 
 const useNotifyMutation = () => {
-  const notify = useMutation(gql`
+  const [notify] = useMutation(gql`
     mutation NotifyMutation($input: NotifyInput!) {
       notify(input: $input) @client {
         id
