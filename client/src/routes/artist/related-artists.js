970d69bf6023191569184baea3b44201ac7917ba
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 17:07:45 -0700

    Use hooks to load related artists

--- client/src/routes/artist/related-artists.js
+++ client/src/routes/artist/related-artists.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ArtistTile from 'components/ArtistTile'
 import gql from 'graphql-tag'
 import TileGrid from 'components/TileGrid'
-import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/react-hooks'
 
-const RelatedArtists = ({ artistId, children }) => (
-  <Query
-    query={gql`
+const RelatedArtists = ({ artistId, children }) => {
+  const {
+    loading,
+    data: { artist }
+  } = useQuery(
+    gql`
       query RelatedArtistQuery($artistId: ID!) {
         artist(id: $artistId) {
           id
@@ -18,18 +21,18 @@ const RelatedArtists = ({ artistId, children }) => (
       }
 
       ${ArtistTile.fragments.artist}
-    `}
-    variables={{ artistId }}
-  >
-    {({ loading, data: { artist } }) => (
-      <TileGrid minWidth="160px">
-        {loading ||
-          artist.relatedArtists.map(artist => (
-            <ArtistTile key={artist.id} artist={artist} />
-          ))}
-      </TileGrid>
-    )}
-  </Query>
-)
+    `,
+    { variables: { artistId } }
+  )
+
+  return (
+    <TileGrid minWidth="160px">
+      {loading ||
+        artist.relatedArtists.map(artist => (
+          <ArtistTile key={artist.id} artist={artist} />
+        ))}
+    </TileGrid>
+  )
+}
 
 export default RelatedArtists
