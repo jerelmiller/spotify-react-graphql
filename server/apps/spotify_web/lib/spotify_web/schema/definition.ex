f69cf050fca70d27deed73429fa6bff882e8ef1c
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 21:58:15 -0700

    Alias the Schema

--- server/apps/spotify_web/lib/spotify_web/schema/definition.ex
+++ server/apps/spotify_web/lib/spotify_web/schema/definition.ex
@@ -2,7 +2,7 @@ defmodule SpotifyWeb.Schema.Definition do
   defmacro __using__(_) do
     quote do
       use Absinthe.Schema.Notation
-
+      alias SpotifyWeb.Schema
       import SpotifyWeb.Resolvers.Helpers
     end
   end
