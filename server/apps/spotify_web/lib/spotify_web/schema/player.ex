260fc5f565d1f584417469de6949f20c339f46ea
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 22:13:49 -0700

    Add more fields to player

--- server/apps/spotify_web/lib/spotify_web/schema/player.ex
+++ server/apps/spotify_web/lib/spotify_web/schema/player.ex
@@ -1,7 +1,17 @@
 defmodule SpotifyWeb.Schema.Player do
   use SpotifyWeb.Schema.Definition
 
+  enum :media_type do
+    value :ad, as: "ad"
+    value :episode, as: "episode"
+    value :track, as: "track"
+    value :unknown, as: "unknown"
+  end
+
   object :player do
+    @desc "The object type of the currently playing item."
+    field :currently_playing_type, non_null(:media_type)
+
     @desc "The device that is currently active."
     field :device, non_null(:device)
 
@@ -22,6 +32,18 @@ defmodule SpotifyWeb.Schema.Player do
 
     @desc "off, track, context"
     field :repeat_state, non_null(:repeat_state)
+
+    @desc "If shuffle is on or off."
+    field :shuffle_state, non_null(:boolean)
+
+    @desc "If something is currently playing."
+    field :is_playing, non_null(:boolean)
+
+    @desc """
+    The currently playing track. Can be null (e.g. If private session is
+    enabled this will be null).
+    """
+    field :item, :full_track
   end
 
   defp progress(%{progress_ms: progress}, _, _), do: {:ok, progress}
