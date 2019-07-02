1bfae01c4c4d7118e051d30ece89d20a5c0cb41e
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 17:39:49 -0700

    Update Track/More to use hook mutation

--- client/src/components/Track/More.tsx
+++ client/src/components/Track/More.tsx
@@ -1,31 +1,28 @@
 import React, { FC, useContext } from 'react'
 import MoreMenu from '../MoreMenu'
 import copyToClipboard from '../../utils/copyToClipboard'
-import Notify from '../NotifyMutation'
 import TrackContext from './Context'
+import useNotifyMutation from 'hooks/useNotifyMutation'
 
 interface Props {}
 
 const More: FC<Props> = () => {
   const { track } = useContext(TrackContext)
+  const notify = useNotifyMutation()
 
   return (
     <MoreMenu size="1.25rem" align="right">
       <MoreMenu.Item>Remove From Your Library</MoreMenu.Item>
       <MoreMenu.Item>Add to Playlist</MoreMenu.Item>
-      <Notify>
-        {({ notify }) => (
-          <MoreMenu.Item
-            onClick={() =>
-              copyToClipboard(track.link).then(() =>
-                notify({ message: 'Link copied to clipboard' })
-              )
-            }
-          >
-            Copy Song Link
-          </MoreMenu.Item>
-        )}
-      </Notify>
+      <MoreMenu.Item
+        onClick={() =>
+          copyToClipboard(track.link).then(() =>
+            notify({ message: 'Link copied to clipboard' })
+          )
+        }
+      >
+        Copy Song Link
+      </MoreMenu.Item>
     </MoreMenu>
   )
 }
