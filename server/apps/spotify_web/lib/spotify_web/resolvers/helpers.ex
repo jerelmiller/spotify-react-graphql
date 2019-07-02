fc5f6ea230ef061d947284c8578b470713ba80cd
Jerel Miller <jerelmiller@gmail.com>
2019-07-01 20:50:27 -0700

    Handle when body is blank

--- server/apps/spotify_web/lib/spotify_web/resolvers/helpers.ex
+++ server/apps/spotify_web/lib/spotify_web/resolvers/helpers.ex
@@ -4,6 +4,10 @@ defmodule SpotifyWeb.Resolvers.Helpers do
 
   def handle_response(response, path \\ fn body -> body end)
 
+  def handle_response({:ok, %HTTPoison.Response{body: ""}}, _) do
+    {:ok, %{}}
+  end
+
   def handle_response({:ok, %HTTPoison.Response{body: body}}, result_path) do
     {:ok, result_path.(body)}
   end
