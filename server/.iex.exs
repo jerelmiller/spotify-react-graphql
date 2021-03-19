2cccd178088b8b9fb21af36e9690b8d7b6b55cdd
Jerel Miller <jmiller@newrelic.com>
2021-03-19 10:32:14 -0700

    add iex helper

---
+++ server/.iex.exs
@@ -0,0 +1,16 @@
+defmodule Helpers do
+  def copy(term) do
+    text =
+      if is_binary(term) do
+        term
+      else
+        inspect(term, limit: :infinity, pretty: true)
+      end
+
+    port = Port.open({:spawn, "pbcopy"}, [])
+    true = Port.command(port, text)
+    true = Port.close(port)
+
+    :ok
+  end
+end
