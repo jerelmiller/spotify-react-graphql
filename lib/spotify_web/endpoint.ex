e60e385d5c1803397f05175e65825cedc85c59de
Jerel Miller <jerelmiller@gmail.com>
2021-12-15 00:32:12 -0800

    Add socket config

--- lib/spotify_web/endpoint.ex
+++ lib/spotify_web/endpoint.ex
@@ -11,6 +11,10 @@ defmodule SpotifyWeb.Endpoint do
     signing_salt: "DrD7oRbM"
   ]
 
+  socket "/socket", SpotifyWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
