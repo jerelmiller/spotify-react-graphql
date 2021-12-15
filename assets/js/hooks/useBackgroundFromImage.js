22e442fd8352619da9182bf2dd83b7ac9a754996
Jerel Miller <jerelmiller@gmail.com>
2021-12-15 02:18:47 -0800

    Dont use color thief for now

--- assets/js/hooks/useBackgroundFromImage.js
+++ assets/js/hooks/useBackgroundFromImage.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import useBackgroundColor from './useBackgroundColor'
-import ColorThief from 'color-thief'
+// import ColorThief from 'color-thief'
 import { rgb, shade } from 'polished'
 
-const useBackgroundFromImage = src => {
+const useBackgroundFromImage = (src) => {
   const [color, setColor] = useState()
   useBackgroundColor(color)
 
@@ -13,9 +13,9 @@ const useBackgroundFromImage = src => {
     if (src) {
       img.crossOrigin = true
       img.onload = () => {
-        const colorThief = new ColorThief()
-        const color = rgb(...colorThief.getColor(img))
-        setColor(shade(0.3, color))
+        // const colorThief = new ColorThief()
+        // const color = rgb(...colorThief.getColor(img))
+        // setColor(shade(0.3, color))
       }
       img.src = src
     }
