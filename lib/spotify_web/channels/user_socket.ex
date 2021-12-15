49d9127f729b900d9ca9d16a72320e99b7299079
Jerel Miller <jerelmiller@gmail.com>
2021-12-15 00:33:44 -0800

    Create the user socket

---
+++ lib/spotify_web/channels/user_socket.ex
@@ -0,0 +1,51 @@
+defmodule SpotifyWeb.UserSocket do
+  use Phoenix.Socket
+  use Absinthe.Phoenix.Socket, schema: SpotifyWeb.Schema
+
+  # A Socket handler
+  #
+  # It's possible to control the websocket connection and
+  # assign values that can be accessed by your channel topics.
+
+  ## Channels
+  # Uncomment the following line to define a "room:*" topic
+  # pointing to the `SpotifyWeb.RoomChannel`:
+  #
+  # channel "room:*", SpotifyWeb.RoomChannel
+  #
+  # To create a channel file, use the mix task:
+  #
+  #     mix phx.gen.channel Room
+  #
+  # See the [`Channels guide`](https://hexdocs.pm/phoenix/channels.html)
+  # for futher details.
+
+  # Socket params are passed from the client and can
+  # be used to verify and authenticate a user. After
+  # verification, you can put default assigns into
+  # the socket that will be set for all channels, ie
+  #
+  #     {:ok, assign(socket, :user_id, verified_user_id)}
+  #
+  # To deny connection, return `:error`.
+  #
+  # See `Phoenix.Token` documentation for examples in
+  # performing token verification on connect.
+  @impl true
+  def connect(%{"token" => token}, socket, _connect_info) do
+    {:ok, Absinthe.Phoenix.Socket.put_options(socket, context: %{authorization: token})}
+  end
+
+  # Socket id's are topics that allow you to identify all sockets for a given user:
+  #
+  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
+  #
+  # Would allow you to broadcast a "disconnect" event and terminate
+  # all active sockets and channels for a given user:
+  #
+  #     Elixir.SpotifyWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #
+  # Returning `nil` makes this socket anonymous.
+  @impl true
+  def id(_socket), do: nil
+end
